1e8410c1fb77819a665d071bef88e353b0d996c2
raguero </SUlem@2017/>
2022-05-24 14:03:45 -0300

    [IMP][onsc_cv_digital]: Tipos de Documentos
    Nueva funcionalidad para el Catálogo paquete completo
    Incluye seguridad
    Data demo
    
    PS07 9857

---
+++ onsc_cv_digital/models/onsc_cv_document_type.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class ONSCCVDocumentType(models.Model):
+    _name = 'onsc.cv.document.type'
+    _description = 'Tipo de Documento'
+
+    code = fields.Char(string=u"Código")
+    name = fields.Char(string=u"Descripción", required=True)
+    active = fields.Boolean(string="Activo", default=True)
+    code_other = fields.Char(string=u"Otro código")
+    is_org = fields.Boolean(u'Aplica Organismo')
+    is_sice = fields.Boolean(u'Aplica SICE')
+    code_sice = fields.Char(u'Código SICE')
+    is_rupe = fields.Boolean(u'Aplica RUPE')
+    code_rupe = fields.Char(u'Código RUPE')
+    is_dgi = fields.Boolean(u'Aplica DGI')
+    code_dgi = fields.Char(u'Código DGI')
+
+    _sql_constraints = [
+        ('name_uniq', 'unique(name)',
+         u'El nombre del Tipo de Documento debe ser único!'),
+    ]
