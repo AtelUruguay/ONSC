ce6b549f7465cc0704e17024cce01e53fb2484fe
Rafael Aguero <rafael.aguero@quanam.com>
2022-05-27 12:13:21 -0300

    Update onsc_cv_institution.py

--- onsc_cv_digital/models/onsc_cv_institution.py
+++ onsc_cv_digital/models/onsc_cv_institution.py
@@ -6,12 +6,13 @@ from odoo import api, fields, models
 class ONSCCVIntitution(models.Model):
     _name = 'onsc.cv.institution'
     _description = 'Institución'
+    _rec_name = 'name_country'
     _inherit = ['onsc.cv.abstract.config']
 
     name = fields.Char("Nombre de la Institución", required=True)
     country_id = fields.Many2one('res.country', string=u'País', ondelete='restrict', required=True)
     enable_mec = fields.Boolean(string=u'Habilitada por el MEC')
-    name_country = fields.Char("Nombre y país de la institución", compute='_compute_name_country_id', readonly=True,
+    name_country = fields.Char("Nombre y país de la institución", compute='_compute_name_country_id',
                                store=True)
     sub_institution_id = fields.One2many('onsc.cv.subinstitution', 'institution_id', string=u"Sub institución")
 
