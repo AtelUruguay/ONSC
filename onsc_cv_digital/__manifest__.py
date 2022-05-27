24823a5edd2a6dba4a209ef5314a1f6ac1b87eb7
raguero </SUlem@2017/>
2022-05-27 15:35:37 -0300

    [ci/cd]

--- onsc_cv_digital/__manifest__.py
+++ onsc_cv_digital/__manifest__.py
@@ -27,7 +27,7 @@ ONSC CV Digital
         'views/res_country_views.xml',
         'views/onsc_cv_subinstitution_views.xml',
         'views/onsc_cv_institution_views.xml',
-		'views/onsc_cv_academic_program_views.xml',
+        'views/onsc_cv_academic_program_views.xml',
         'views/onsc_cv_menuitems.xml',
     ],
     'demo': [
