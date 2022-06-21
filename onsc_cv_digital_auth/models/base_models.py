1ec8e9248bb5e7c1004deeb69859b9c0a8e59c92
rpelaezg87 <rpelaezg87@gmail.com>
2022-06-21 12:57:55 -0300

    [FIX] [onsc_cv_digital_auth] Lógica de actualización de campos de ID digital modificada Habilitando el write de res.partner para realizar el calculo de los de de ID digital

--- onsc_cv_digital_auth/models/base_models.py
+++ onsc_cv_digital_auth/models/base_models.py
@@ -21,9 +21,12 @@ def compare_string_without_consider_accents(str1='', str2=''):
     :param str2:
     :return:
     """
-    upper_str1 = unidecode(str1.upper())
-    upper_str2 = unidecode(str2.upper())
-    return upper_str1 == upper_str2
+    if all([str1, str2]):
+        upper_str1 = unidecode(str1.upper())
+        upper_str2 = unidecode(str2.upper())
+        return upper_str1 == upper_str2
+    # Si alguno de los términos es vacio retorna False
+    return False
 
 
 class ResUsers(models.Model):
@@ -71,7 +74,7 @@ class ResPartner(models.Model):
         super(ResPartner, self)._compute_cv_full_name()
 
     def button_update_dnic_values(self):
-        self.update_dnic_values()
+        self.with_context(call_from_button=True).update_dnic_values()
 
     def get_cv_main_values(self, response):
         "Metodo que arma el diccionario los valores correspondientes a actualizar de ID digital "
@@ -86,39 +89,75 @@ class ResPartner(models.Model):
                            cv_last_name_2]
             return ' '.join([x for x in name_values if x])
 
-        cv_full_name = calc_full_name(
-            self.cv_first_name,
-            self.cv_second_name,
-            self.cv_last_name_1,
-            self.cv_last_name_2)
+        if self._context.get('call_from_button'):
+            cv_dnic_name_1 = response.get('cv_dnic_name_1', '')
+            cv_dnic_name_2 = response.get('cv_dnic_name_2', '')
+            cv_dnic_lastname_1 = response.get('cv_dnic_lastname_1', '')
+            cv_dnic_lastname_2 = response.get('cv_dnic_lastname_2', '')
+            cv_dnic_full_name = response.get('cv_dnic_full_name', '')
+            cv_last_name_adoptive_1 = response.get('cv_last_name_adoptive_1', '')
+            cv_last_name_adoptive_2 = response.get('cv_last_name_adoptive_2', '')
+        else:
+            # Si no se llama desde el boton y se pulsa guardar entonces tomamos los datos ya registrados
+            cv_dnic_name_1 = response.get('cv_dnic_name_1', self.cv_dnic_name_1)
+            cv_dnic_name_2 = response.get('cv_dnic_name_2', self.cv_dnic_name_2)
+            cv_dnic_lastname_1 = response.get('cv_dnic_lastname_1', self.cv_dnic_lastname_1)
+            cv_dnic_lastname_2 = response.get('cv_dnic_lastname_2', self.cv_dnic_lastname_2)
+            cv_dnic_full_name = response.get('cv_dnic_full_name', self.cv_dnic_full_name)
+            cv_last_name_adoptive_1 = response.get('cv_last_name_adoptive_1', self.cv_last_name_adoptive_1)
+            cv_last_name_adoptive_2 = response.get('cv_last_name_adoptive_2', self.cv_last_name_adoptive_2)
+
+        cv_full_name = calc_full_name(cv_dnic_name_1, cv_dnic_name_2, cv_dnic_lastname_1, cv_dnic_lastname_2)
 
         # Caso 1: Ambos Nombres y Apellidos coinciden con nombre en cédula
-        if compare_string_without_consider_accents(cv_full_name, response.get('cv_dnic_full_name')):
+        if compare_string_without_consider_accents(cv_full_name, cv_dnic_full_name):
             # Caso 1.b: Apellido1 nulo
-            if (not self.cv_last_name_1 and self.cv_last_name_2):
+            result.update({
+                'cv_first_name': cv_dnic_name_1,
+                'cv_second_name': cv_dnic_name_2,
+            })
+            if not cv_dnic_lastname_1 and cv_dnic_lastname_2:
                 result.update({
-                    'cv_last_name_1': self.cv_last_name_2,
-                    'cv_last_name_2': self.cv_last_name_1,
-                })
-        else:
-            cv_full_name = calc_full_name(self.cv_first_name, self.cv_second_name, self.cv_last_name_2,
-                                          self.cv_last_name_1)
-            # Caso 2: Apellidos cambiados de orden
-            if compare_string_without_consider_accents(cv_full_name, response.get('cv_dnic_full_name')):
-                result.update({
-                    'cv_last_name_1': self.cv_last_name_2,
-                    'cv_last_name_2': self.cv_last_name_1,
+                    'cv_last_name_1': cv_dnic_lastname_2,
+                    'cv_last_name_2': cv_dnic_lastname_1
                 })
             else:
-                cv_last_name_adoptive_1 = response.get('cv_last_name_adoptive_1', '')
-                cv_last_name_adoptive_2 = response.get('cv_last_name_adoptive_2', '')
+                result.update({
+                    'cv_last_name_1': cv_dnic_lastname_1,
+                    'cv_last_name_2': cv_dnic_lastname_2,
+                })
+
+        else:
+            # Caso 2: Apellidos cambiados de orden
+            cv_full_name = calc_full_name(cv_dnic_name_1, cv_dnic_name_2, cv_dnic_lastname_2, cv_dnic_lastname_1)
+            if compare_string_without_consider_accents(cv_full_name, cv_dnic_full_name):
+                result.update({
+                    'cv_first_name': cv_dnic_name_1,
+                    'cv_second_name': cv_dnic_name_2,
+                    'cv_last_name_1': cv_dnic_lastname_2,
+                    'cv_last_name_2': cv_dnic_lastname_1,
+                })
+            else:
+
                 if cv_last_name_adoptive_1 or cv_last_name_adoptive_2:
-                    cv_full_name = calc_full_name(self.cv_first_name, self.cv_second_name, cv_last_name_adoptive_1,
+                    cv_full_name = calc_full_name(cv_dnic_name_1, cv_dnic_name_2, cv_last_name_adoptive_1,
                                                   cv_last_name_adoptive_2)
-                    # Caso 3: Apellidos adoptivos en nombre en nombre en cédula
-                    if compare_string_without_consider_accents(cv_full_name, response.get('cv_dnic_full_name')):
+                    # Caso 3: Apellidos adoptivos en nombre en cédula
+                    if compare_string_without_consider_accents(cv_full_name, cv_dnic_full_name):
                         result.update({
+                            'cv_first_name': cv_dnic_name_1,
+                            'cv_second_name': cv_dnic_name_2,
                             'cv_last_name_1': cv_last_name_adoptive_1,
                             'cv_last_name_2': cv_last_name_adoptive_2,
                         })
         return result
+
+    def write(self, values):
+        if values is None:
+            values = {}
+        # Se agrega el código para habilitar el calculo de los campos de ID digital al guardar el formulario
+        # Esto posibilita realizar pruebas a la lógica
+        if not self._context.get('call_from_button', False) \
+                and list(set(DNIC_FROZEN_COLUMNS).intersection(set(values.keys()))):
+            values.update(self.get_cv_main_values(values))
+        return super(ResPartner, self).write(values)
