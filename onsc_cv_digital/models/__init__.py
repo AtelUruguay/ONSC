6482cb6a3d0323cfb89289f97654b3fb051bfb09
dmontero <dmontero@quanam.com>
2022-07-06 14:53:12 -0300

    [FIX][onsc_cv_digital] "Experiencia Laboral" mejoras en el codigo e integracion con clases abstractas

--- onsc_cv_digital/models/__init__.py
+++ onsc_cv_digital/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from . import abstracts
 from . import res_company
 from . import res_config_settings
 from . import catalogs
@@ -7,3 +8,4 @@ from . import onsc_cv_digital
 from . import onsc_cv_digital_general_info
 from . import onsc_cv_digital_work_experience
 from . import formation
+
