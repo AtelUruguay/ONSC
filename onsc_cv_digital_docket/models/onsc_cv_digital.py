6738da6a1bef581da5d18fe083930c230ec29633
raguero </SUlem@2017/>
2022-07-12 12:01:38 -0300

    [FIX][onsc_cv_digital] Correcciones PS07 9933

--- onsc_cv_digital_docket/models/onsc_cv_digital.py
+++ onsc_cv_digital_docket/models/onsc_cv_digital.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ONSCCVDigital(models.Model):
     _inherit = 'onsc.cv.digital'
 
     is_docket = fields.Boolean(string="Tiene legajo")
-    gender_date = fields.Date(string="Fecha de información Género")
-    gender_public_visualization_date = fields.Date(string="Fecha información visualización pública de género")
+    gender_date = fields.Date(string="Fecha de información género")
+    gender_public_visualization_date = fields.Date(string="Fecha información visualización pública de género",
+                                                   compute='_compute_gender_public_visualization_date', store=True)
     afro_descendant_date = fields.Date(string="Fecha de información afrodescendencia")
-    status_civil_date = fields.Date(string="Fecha de información stado civil")
+    status_civil_date = fields.Date(string="Fecha de información estado civil")
+
+    @api.depends('is_cv_gender_public')
+    def _compute_gender_public_visualization_date(self):
+        for record in self:
+            record.gender_public_visualization_date = fields.Date.today()
+
