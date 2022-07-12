136c70dfafaf280fd868aede73ac6282f203c8ba
raguero </SUlem@2017/>
2022-07-12 14:02:59 -0300

    [ci/cd][onsc_cv_digital]

--- onsc_cv_digital/migrations/15.0.2.0.1/pre-migrate-abstract-origin-institution.py
+++ onsc_cv_digital/migrations/15.0.2.0.1/pre-migrate-abstract-origin-institution.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 def migrate(cr, version):
     # Delete model onsc.cv.abstract.origin.institution because change name to onsc.cv.abstract.work
     cr.execute("delete from ir_model where model='onsc.cv.abstract.origin.institution'")
