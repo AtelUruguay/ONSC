e787d081e780a34e2266f00345142ec180ed13ed
rpelaezg87 <rpelaezg87@gmail.com>
2022-07-21 18:15:30 -0300

    [ADD]onsc_cv_digital CVDigital Arreglo en campo Fecha de información patronímica (se agrega un default al campo) ps07: 9934

--- onsc_cv_digital/models/onsc_cv_digital_reference.py
+++ onsc_cv_digital/models/onsc_cv_digital_reference.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 from .onsc_cv_useful_tools import is_valid_email
 from .onsc_cv_useful_tools import get_onchange_warning_response as cv_warning
 
@@ -13,7 +14,8 @@ class ONSCCVDigitalReference(models.Model):
     cv_digital_id = fields.Many2one("onsc.cv.digital", string="CV", required=True, index=True, ondelete='cascade')
     reference_type = fields.Selection(REFERENCE_TYPES, 'Tipo de referencia', required=True)
     name = fields.Char('Nombre', required=True)
-    country_id = fields.Many2one('res.country', 'Pais', required=True)
+    country_id = fields.Many2one('res.country', 'Pais', required=True,
+                                 default=lambda self: self.env['res.country'].search([('code', '=', 'UY')]))
     phone = fields.Char('Teléfono')
     phone_full = fields.Char(compute='_compute_phone_full')
     email = fields.Char('Mail')
@@ -30,3 +32,14 @@ class ONSCCVDigitalReference(models.Model):
         if self.email and not is_valid_email(self.email):
             self.email = False
             return cv_warning(_('El mail ingresado no tiene un formato válido'))
+
+    @api.onchange('phone')
+    def onchange_phone(self):
+        if self.phone and not self.phone.isdigit():
+            return cv_warning(_("El teléfono ingresado no es válido"))
+
+    @api.constrains('phone')
+    def check_phone(self):
+        for rec in self:
+            if rec.phone and not rec.phone.isdigit():
+                raise ValidationError(_("El teléfono ingresado no es válido"))
