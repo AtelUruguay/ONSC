56c842bde1b9c450321ec1f61dd8e2e5f05f96c2
abe <abelardolopezquintana@gmail.com>
2022-07-21 19:41:25 -0500

    [Fix][onsc_cv_digita] Cprreciones de string y arreglos de radios

--- onsc_cv_digital/models/onsc_cv_digital.py
+++ onsc_cv_digital/models/onsc_cv_digital.py
@@ -12,7 +12,7 @@ HTML_HELP = """<a     class="btn btn-outline-dark" target="_blank" title="Enlace
 SELECTION_RADIO = [('1', '1) Si, no puede hacerlo'), ('2', '2) Si, mucha dificultad'),
                    ('3', '3) Si, alguna dificultad '), ('4', '4) No tiene dificultad')]
 SITUATION = u'Está en situación de discapacidad y/o requieres algún apoyo para cumplir con tus actividades laborales?'
-DISABILITE = u'¿Está inscripto en el registro de personas con discapacidad del Ministerio de Desarrollo Social?'
+DISABILITE = u'¿Está inscripto en el registro de personas con discapacidad del ministerio de desarrollo social?'
 
 
 class ONSCCVDigital(models.Model):
@@ -212,7 +212,7 @@ class ONSCCVDigital(models.Model):
                                             string=u'¿Permite que el contenido de esta sección sea público?')
     situation_disability = fields.Selection(selection=[('si', u'Si'), ('no', u'No')], string=SITUATION)
     people_disabilitie = fields.Selection(selection=[('si', u'Si'), ('no', u'No')], string=DISABILITE)
-    document_certificate_file = fields.Binary(string=u'Documento Digitalizado Constancia de inscripción en el RNPcD')
+    document_certificate_file = fields.Binary(string=u'Documento digitalizado constancia de inscripción en el RNPcD')
     document_certificate_filename = fields.Char('Nombre del documento Digitalizado')
     certificate_date = fields.Date(string=u'Fecha de certificado', required=True)
     to_date = fields.Date(string=u'Fecha hasta')
@@ -236,7 +236,7 @@ class ONSCCVDigital(models.Model):
     # Otra información relevante ----<Page>
     other_relevant_information_ids = fields.One2many("onsc.cv.other.relevant.information",
                                                      inverse_name="cv_digital_id",
-                                                     string="Otra información Relevante")
+                                                     string="Otra información relevante")
     # Help online
     cv_help_general_info = fields.Html(
         compute=lambda s: s._get_help('cv_help_general_info'),
