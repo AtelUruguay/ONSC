3e9dd6fdda4d1f3f2e660a3265de94f977a35b98
rpelaezg87 <rpelaezg87@gmail.com>
2022-07-29 15:31:11 -0300

    [ADD][onsc_cv_digital] Pestaña Experiencia laboral agregando Empresa (Concatenando Inciso+UE+Nombre de empresa) Pestaña Voluntariado agregando Empresa (Concatenando Inciso+UE+Nombre de empresa) ps07:9947

--- onsc_cv_digital_catalog/models/onsc_cv_digital.py
+++ onsc_cv_digital_catalog/models/onsc_cv_digital.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ONSCCVDigitalWorkExperience(models.Model):
@@ -10,6 +10,16 @@ class ONSCCVDigitalWorkExperience(models.Model):
     operating_unit_id = fields.Many2one("operating.unit",
                                         string="Unidad ejecutora",
                                         domain=[('inciso_id', '=', inciso_id)])
+    company_name_clac = fields.Char('Empresa', compute='_compute_company_name_clac')
+
+    @api.depends('inciso_id', 'operating_unit_id', 'company_name')
+    def _compute_company_name_clac(self):
+        for rec in self:
+            if rec.inciso_id and rec.operating_unit_id and rec.company_name:
+                rec.company_name_clac = '%s-%s-%s' % (
+                    rec.inciso_id.short_name, rec.operating_unit_id.budget_code, rec.company_name)
+            else:
+                rec.company_name_clac = rec.company_name
 
 
 class ONSCCVDigitalVolunteering(models.Model):
@@ -19,3 +29,13 @@ class ONSCCVDigitalVolunteering(models.Model):
     operating_unit_id = fields.Many2one("operating.unit",
                                         string="Unidad ejecutora",
                                         domain=[('inciso_id', '=', inciso_id)])
+    company_name_clac = fields.Char('Empresa', compute='_compute_company_name_clac')
+
+    @api.depends('inciso_id', 'operating_unit_id', 'company_name')
+    def _compute_company_name_clac(self):
+        for rec in self:
+            if rec.inciso_id and rec.operating_unit_id and rec.company_name:
+                rec.company_name_clac = '%s-%s-%s' % (
+                    rec.inciso_id.short_name, rec.operating_unit_id.budget_code, rec.company_name)
+            else:
+                rec.company_name_clac = rec.company_name
