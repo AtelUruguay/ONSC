60cf2ceb8643df54e7e8c921fbf52716e7c66c53
abe <abelardolopezquintana@gmail.com>
2022-08-01 21:48:55 -0500

    [Fix][onsc_cv_digital_docket] Solo será visible para los que tengan tienen Legajo activo

--- onsc_cv_digital_docket/models/onsc_cv_digital.py
+++ onsc_cv_digital_docket/models/onsc_cv_digital.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models, api, _
 from odoo.addons.onsc_cv_digital.models.onsc_cv_useful_tools import get_onchange_warning_response as cv_warning
+
+from odoo import fields, models, api, _
 from odoo.exceptions import ValidationError
 
 BLOOD_TYPE = [('unknown', 'Desconocido'), ('O+', 'O+'), ('A+', 'A+'), ('B+', 'B+'), ('AB+', 'AB+'), ('O-', 'O-'),
@@ -15,6 +16,7 @@ class ONSCCVDigital(models.Model):
     _inherit = 'onsc.cv.digital'
 
     is_docket = fields.Boolean(string="Tiene legajo")
+    is_docket_active = fields.Boolean(string="Tiene legajo activo")
     gender_date = fields.Date(string="Fecha de información género")
     gender_public_visualization_date = fields.Date(string="Fecha información visualización pública de género",
                                                    compute='_compute_gender_public_visualization_date', store=True)
@@ -27,16 +29,16 @@ class ONSCCVDigital(models.Model):
     disability_date = fields.Date(string="Fecha de información discapacidad")
     # Datos del Legajo ----<Page>
     institutional_email = fields.Char(string=u'Correo electrónico institucional', readonly=True)
-    digitized_document_file = fields.Binary(string=digitized_document_full_name, required=True)
+    digitized_document_file = fields.Binary(string=digitized_document_full_name)
     digitized_document_filename = fields.Char('Nombre del documento Digitalizado')
     # TO-DO: Revisar este campo, No esta en catalogo
-    # mobile_mergency_service_id = fields.Many2one("model", u"Servicio de emergencia móvil", required=True)
-    emergency_service_telephone = fields.Char(string=u'Teléfono del servicio de emergencia', required=True)
+    # mobile_mergency_service_id = fields.Many2one("model", u"Servicio de emergencia móvil")
+    emergency_service_telephone = fields.Char(string=u'Teléfono del servicio de emergencia')
     department_id = fields.Many2one('res.country.state', string=u'Departamento del prestador de salud',
-                                    ondelete='restrict', required=True, tracking=True)
+                                    ondelete='restrict', tracking=True)
     # TO-DO: Revisar este campo, No esta en catalogo
-    # health_provider_id = fields.Many2one("model", u"Prestador de Salud", required=True)
-    blood_type = fields.Selection(BLOOD_TYPE, string=u'Tipo de sangre', required=True)
+    # health_provider_id = fields.Many2one("model", u"Prestador de Salud")
+    blood_type = fields.Selection(BLOOD_TYPE, string=u'Tipo de sangre')
     information_contact_ids = fields.One2many('onsc.cv.information.contact', 'cv_digital_id',
                                               string=u'Información de Contacto')
     other_information_official = fields.Text(string="Otra información del funcionario/a")
