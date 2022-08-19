36b7e18e6152969f898f0d71b4d678f3986c4436
dmontero <dmontero@quanam.com>
2022-08-19 11:14:23 -0300

    [IMP][onsc_cv_digital] CAT_1.10_01 Crear catálogo Descriptor1,CAT_1.11_01 Crear catálogo Descriptor2,CAT_1.12_01 Crear catálogo Descriptor3,CAT_1.13_01 Crear catálogo Descriptor4. Nuevos modelos y menus task 10119

--- onsc_catalog/models/catalogs/onsc_catalog_base.py
+++ onsc_catalog/models/catalogs/onsc_catalog_base.py
@@ -84,20 +84,23 @@ class ONSCCatalogDescriptor1(models.Model):
     _description = u'Descriptor 1'
     _inherit = ['onsc.cv.catalog.abstract']
 
-    occupational_family_id = fields.Many2one("onsc.catalog.occupational.family", string="Mapeo Familia Ocupacional",ondelete='restrict')
+    occupational_family_id = fields.Many2one("onsc.catalog.occupational.family", string="Mapeo Familia Ocupacional",
+                                             ondelete='restrict')
+
 
 class ONSCCatalogDescriptor2(models.Model):
     _name = 'onsc.catalog.descriptor2'
     _description = u'Descriptor 2'
     _inherit = ['onsc.cv.catalog.abstract']
 
+
 class ONSCCatalogDescriptor3(models.Model):
     _name = 'onsc.catalog.descriptor3'
     _description = u'Descriptor 3'
     _inherit = ['onsc.cv.catalog.abstract']
 
+
 class ONSCCatalogDescriptor4(models.Model):
     _name = 'onsc.catalog.descriptor4'
     _description = u'Descriptor 4'
     _inherit = ['onsc.cv.catalog.abstract']
-
