5e39b0ee079703c9ac49f039b98670615b09b277
dmontero <dmontero@quanam.com>
2022-08-26 12:30:27 -0300

    [IMP][onsc_cv_digital_catalog] CVD_3.2.01 Validadores por Inciso y UE. CVD_5.6.01 Crear nuevo grupo de Seguridad “Administrador de llamado” task 9905

--- onsc_cv_digital_catalog/models/onsc_catalog_validators_inciso_ue.py
+++ onsc_cv_digital_catalog/models/onsc_catalog_validators_inciso_ue.py
@@ -3,6 +3,7 @@
 from odoo import fields, models, api, _
 from odoo.exceptions import ValidationError
 
+
 class ONSCCatalogValidatorsIncisoUE(models.Model):
     _name = 'onsc.catalog.validators.inciso.ue'
     _description = 'Validadores por Inciso y UE'
@@ -12,7 +13,7 @@ class ONSCCatalogValidatorsIncisoUE(models.Model):
     operating_unit_id = fields.Many2one("operating.unit", string="Unidad ejecutora",
                                         ondelete='restrict',
                                         history=True)
-    user_id = fields.Many2one('res.users', 'Usuario',history=True)
+    user_id = fields.Many2one('res.users', 'Usuario', history=True)
     security = fields.Char(string="Seguridad", compute='_compute_security')
     active = fields.Boolean(default=True, history=True)
 
@@ -21,7 +22,7 @@ class ONSCCatalogValidatorsIncisoUE(models.Model):
             validator = self.env['ir.config_parameter'].sudo().get_param("Validador documental llamados", False)
             record.security = validator if validator else ""
 
-    @api.constrains('inciso_id', 'operating_unit_id','user_id')
+    @api.constrains('inciso_id', 'operating_unit_id', 'user_id')
     def _check_valid(self):
         for record in self:
             if not record.personal_phone and not record.mobile_phone:
