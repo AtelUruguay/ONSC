df05ed5e2cbe5640ac0c08aeb73c1373f2bfe4a2
raguero </SUlem@2017/>
2022-09-02 10:32:28 -0300

    [ADD][onsc_cv_digital] WS Postulación: FIXES

--- onsc_cv_digital/soap/soap_error_codes.py
+++ onsc_cv_digital/soap/soap_error_codes.py
@@ -33,7 +33,7 @@ AUTH_52 = {
 AUTH_53 = {
     'type': 'AUTH',
     'code': '53',
-    'desc': u'DBNAME inválido o inexistente',
+    'desc': u'DBNAME inexistente',
     'long_desc': u'Contacte con el administrador',
 }
 
@@ -50,6 +50,13 @@ LOGIC_151 = {
     'code': '151',
     'desc': u'Código del País no identificado',
     'long_desc': u'',
+}
+
+LOGIC_151_1 = {
+    'type': 'LOGICA',
+    'code': '151',
+    'desc': u'Código del País no identificado',
+    'long_desc': u'Fórmato inválido, la longitud adecuada es 2',
 }
 
 LOGIC_152 = {
