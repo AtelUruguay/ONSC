970d9044f89ac4ce46129227d60e3e6a707acf4b
raguero </SUlem@2017/>
2022-09-21 11:51:45 -0300

    [FIX, ADD][onsc_cv_digital] Validadores Inciso UE

--- onsc_cv_digital_catalog/models/onsc_catalog_validators_inciso_ue.py
+++ onsc_cv_digital_catalog/models/onsc_catalog_validators_inciso_ue.py
@@ -7,18 +7,18 @@ from odoo.exceptions import ValidationError
 class ONSCCatalogValidatorsIncisoUE(models.Model):
     _name = 'onsc.catalog.validators.inciso.ue'
     _description = 'Validadores por Inciso y UE'
+    _rec_name = 'user_id'
 
-    inciso_id = fields.Many2one('onsc.catalog.inciso', string='Inciso', required=True, ondelete='restrict', index=True)
+    inciso_id = fields.Many2one('onsc.catalog.inciso', string='Inciso', required=True, ondelete='restrict')
     operating_unit_id = fields.Many2one("operating.unit", string="Unidad ejecutora",
-                                        ondelete='restrict',
-                                        history=True)
+                                        ondelete='restrict')
     user_id = fields.Many2one('res.users', 'Usuario')
-    security = fields.Char(string="Seguridad", compute='_compute_security')
-    active = fields.Boolean(default=True)
+    group_id = fields.Many2one('res.groups', string='Seguridad', default=lambda self: self.get_default_group_id())
 
-    def _compute_security(self):
-        for record in self:
-            record.security = self.env['ir.config_parameter'].sudo().get_param("called_document_validator", "")
+    @api.model
+    def get_default_group_id(self):
+        security_param = self.env['ir.config_parameter'].sudo().get_param("group_documentary_validator", "")
+        return self.env['res.groups'].search([('name', '=', security_param)]).id
 
     @api.constrains('inciso_id', 'operating_unit_id', 'user_id')
     def _check_valid(self):
@@ -27,3 +27,17 @@ class ONSCCatalogValidatorsIncisoUE(models.Model):
                     [('inciso_id', '=', record.inciso_id.id), ('operating_unit_id', '=', record.operating_unit_id.id),
                      ('user_id', '=', record.user_id.id), ('id', '!=', record.id)]):
                 raise ValidationError(_("Ya existe un usuario configurado para ese Inciso y UE"))
+
+    @api.model
+    def create(self, values):
+        new_recordset = super(ONSCCatalogValidatorsIncisoUE, self).create(values)
+        if new_recordset.user_id.id not in new_recordset.group_id.users.ids:
+            new_recordset.user_id.suspend_security().write({
+                'groups_id': [(4, new_recordset.group_id.id)]
+            })
+        return new_recordset
+
+    def unlink(self):
+        for record in self:
+            record.user_id.write({'groups_id': [(3, record.group_id.id)]})
+        return super(ONSCCatalogValidatorsIncisoUE, self).unlink()
