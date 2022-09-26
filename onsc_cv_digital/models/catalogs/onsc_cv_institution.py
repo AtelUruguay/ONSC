b3b45d584bbac06940bf9c1ec48c35dde77579da
david <davidmonterocrespo24@gmail.com>
2022-09-26 10:40:03 -0300

    [fix][onsc_cv_digital] Nuevos requerimientos pestaña formación- check para instituciones task: 10446
    Signed-off-by: david <davidmonterocrespo24@gmail.com>

--- onsc_cv_digital/models/catalogs/onsc_cv_institution.py
+++ onsc_cv_digital/models/catalogs/onsc_cv_institution.py
@@ -17,8 +17,8 @@ class ONSCCVIntitution(models.Model):
                                store=True)
     subinstitution_ids = fields.One2many('onsc.cv.subinstitution', 'institution_id', string=u"Sub institución",
                                          tracking=True)
-    advanced_formation = fields.Boolean(string=u'Formación avanzada', tracking=True)
-    basic_formation = fields.Boolean(string=u'Formación básica', tracking=True)
+    is_advanced_formation = fields.Boolean(string=u'Formación avanzada', tracking=True)
+    is_basic_formation = fields.Boolean(string=u'Formación básica', tracking=True)
 
     @api.depends('name', 'country_id')
     def _compute_name_country_id(self):
