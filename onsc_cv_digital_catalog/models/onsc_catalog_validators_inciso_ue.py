ad90fec9a0b6c7fd1380811a0be29dc763f32d3f
raguero </SUlem@2017/>
2022-10-04 13:08:22 -0300

    [FIX][onsc_cv_digital] Reordenamiento y mejoras varias

--- onsc_cv_digital_catalog/models/onsc_catalog_validators_inciso_ue.py
+++ onsc_cv_digital_catalog/models/onsc_catalog_validators_inciso_ue.py
@@ -45,30 +45,26 @@ class ONSCCatalogValidatorsIncisoUE(models.Model):
     @api.model
     def create(self, values):
         new_recordset = super(ONSCCatalogValidatorsIncisoUE, self).create(values)
-        user_seg = self.search_count([("user_id", "=", self.user_id.id)])
-        if user_seg == 1:
-            if new_recordset.user_id.id not in new_recordset.group_id.users.ids:
-                new_recordset.user_id.suspend_security().write({
-                    'groups_id': [(4, new_recordset.group_id.id)]
-                })
+        if new_recordset.user_id.id not in new_recordset.group_id.users.ids:
+            new_recordset.user_id.suspend_security().write({
+                'groups_id': [(4, new_recordset.group_id.id)]
+            })
         return new_recordset
 
     def write(self, values):
         user_id = values.get('user_id')
-        user_seg = self.search_count([("user_id", "=", self.user_id.id)])
-        if user_seg == 1:
-            if user_id:
-                for record in self:
-                    record.user_id.suspend_security().write({'groups_id': [(3, record.group_id.id)]})
+        if user_id:
+            self.update_user_security()
         recordsets = super(ONSCCatalogValidatorsIncisoUE, self).write(values)
-        if user_seg == 1:
-            if user_id:
-                record.mapped('user_id').suspend_security().write({'groups_id': [(4, user_id)]})
+        if user_id:
+            self.mapped('user_id').suspend_security().write({'groups_id': [(4, user_id)]})
         return recordsets
 
     def unlink(self):
-        user_seg = self.search_count([("user_id", "=", self.user_id.id)])
-        if user_seg == 1:
-            for record in self:
-                record.user_id.suspend_security().write({'groups_id': [(3, record.group_id.id)]})
+        self.update_user_security()
         return super(ONSCCatalogValidatorsIncisoUE, self).unlink()
+
+    def update_user_security(self):
+        for record in self:
+            if self.search_count([("user_id", "=", record.user_id.id), ('id', '!=', record.id)]) == 0:
+                record.user_id.suspend_security().write({'groups_id': [(3, record.group_id.id)]})
