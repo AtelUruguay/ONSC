1320636df5df049a5e80c5efaf45e02ae506c5bc
raguero </SUlem@2017/>
2022-11-07 16:30:30 -0300

    [ADD] Seguridad: Acción por defecto en nuevos usuarios

--- onsc_base/__init__.py
+++ onsc_base/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import onsc_useful_tools
+from . import models
--- onsc_base/__manifest__.py
+++ onsc_base/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ONSC - Base',
-    'version': '15.0.3.1.0',
+    'version': '15.0.4.0.0',
     'summary': 'ONSC - Base',
     'sequence': 11,
     'description': """
