7436b2bb38a896f0a969732193ebf5fa6258b90c
raguero </SUlem@2017/>
2022-12-29 11:47:59 -0300

    ci/cd

--- onsc_cv_digital/migrations/15.0.8.3.1/pre-migrate-work-teaching.py
+++ onsc_cv_digital/migrations/15.0.8.3.1/pre-migrate-work-teaching.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 def migrate(cr, version):
-    #DROP TABLE many2many  - Asociada a una columna que ya deja de existir en Docencia->Materias
+    # DROP TABLE many2many  - Asociada a una columna que ya deja de existir en Docencia->Materias
     cr.execute("DROP TABLE IF EXISTS academic_program_teaching_rel")
