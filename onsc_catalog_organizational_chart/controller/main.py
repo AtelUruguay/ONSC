6234d233b8f9eee6693b1d476f152af5e90dd7be
raguero </SUlem@2017/>
2023-01-13 14:07:44 -0300

    [ADD][ORGANIGRAMA] Mejoras varias

--- onsc_catalog_organizational_chart/controller/main.py
+++ onsc_catalog_organizational_chart/controller/main.py
@@ -15,19 +15,19 @@ class EmployeeChart(http.Controller):
         domain = [
             ('operating_unit_id', '=', operating_unit_id),
         ]
-        if end_date:
-            domain.extend(
-                [
-                    # '|',
-                    ('end_date', '>=', end_date),
-                    # ('end_date', '=', False),
-
-                ]
-            )
+        # if end_date:
+        #     domain.extend(
+        #         [
+        #             # '|',
+        #             ('end_date', '>=', end_date),
+        #             # ('end_date', '=', False),
+        #
+        #         ]
+        #     )
         levels = request.env['onsc.catalog.hierarchical.level'].sudo().search([])
         if department_id:
-            domain.extend(['|', ('parent_id', '=', int(department_id)), ('id', '=', int(department_id))])
-        nodes_by_level = request.env['hr.department'].sudo().with_context(as_of_date=end_date).search(
+            domain.extend(['|', ('id', 'child_of', int(department_id)), ('id', '=', int(department_id))])
+        nodes_by_level = request.env['hr.department'].sudo().search(
             domain, order='id asc, parent_id asc'
         )
         root_node = nodes_by_level.filtered(
