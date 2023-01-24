74249b728ad24d42606ca82412cb4151b4237de3
raguero </SUlem@2017/>
2023-01-24 13:49:32 -0300

    ci/cd

--- onsc_legajo/models/catalogs/onsc_legajo_office.py
+++ onsc_legajo/models/catalogs/onsc_legajo_office.py
@@ -7,10 +7,8 @@ class ONSCLegajoOffice(models.Model):
     _name = 'onsc.legajo.office'
     _description = 'Oficina'
 
-
     inciso = fields.Many2one(comodel_name="onsc.catalog.inciso", string="Inciso")
     unidadEjecutora = fields.Many2one(comodel_name="operating.unit", string="Unidad ejecutora")
-
 
     active = fields.Boolean(string="Activo", default=True)
 
