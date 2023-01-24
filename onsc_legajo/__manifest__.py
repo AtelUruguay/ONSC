1e7d8eb506cbe61af70aa1e6b86a10a3a51b2e03
david <davidmonterocrespo24@gmail.com>
2023-01-24 10:53:08 -0300

    [FIX][onsc_docket]LEG_1.10_01 Crear catálogo Causales de Egreso
    Signed-off-by: david <davidmonterocrespo24@gmail.com>

--- onsc_legajo/__manifest__.py
+++ onsc_legajo/__manifest__.py
@@ -9,7 +9,7 @@ ONSC - Legajo
 ====================
     """,
     'category': 'ONSC',
-    'depends': ['onsc_base', 'onsc_catalog','base_user_role'],
+    'depends': ['onsc_base', 'onsc_catalog', 'base_user_role'],
     'data': [
         'security/onsc_legajo_security.xml',
         'security/ir.model.access.csv',
