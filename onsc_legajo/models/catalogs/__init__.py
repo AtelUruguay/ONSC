bac66bf3ad5a6e24ecbc957931fc027b23064834
david <davidmonterocrespo24@gmail.com>
2023-01-27 15:39:56 -0300

    CI/CD
    Signed-off-by: david <davidmonterocrespo24@gmail.com>

--- onsc_legajo/models/catalogs/__init__.py
+++ onsc_legajo/models/catalogs/__init__.py
@@ -11,8 +11,6 @@ from . import onsc_legajo_integration_error
 from . import onsc_legajo_reason_extinction_commission
 from . import onsc_legajo_commission_regime
 from . import onsc_legajo_security_job
-from . import onsc_legajo_integration_error
-from . import onsc_legajo_regime
 from . import onsc_legajo_budget_item
 from . import onsc_legajo_norm
 from . import onsc_legajo_office
