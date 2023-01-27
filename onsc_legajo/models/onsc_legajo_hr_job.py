34d066b2fe40befd0d74f65c93e9f9a1db141d30
david <davidmonterocrespo24@gmail.com>
2023-01-27 15:04:19 -0300

    [IMP][onsc_legano]LEG_2.4_01 Ajustar entidad puesto
    Signed-off-by: david <davidmonterocrespo24@gmail.com>

--- onsc_legajo/models/onsc_legajo_hr_job.py
+++ onsc_legajo/models/onsc_legajo_hr_job.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import json
 
 from odoo import fields, models, api, _
 
@@ -67,7 +66,7 @@ class HrJobRoleLine(models.Model):
     start_date = fields.Date(string="Fecha desde")
     end_date = fields.Date(string="Fecha hasta")
     creation_mode_type = fields.Selection([('manual', 'Manual'), ('system', 'Seguridad de Puesto')],
-                                     string='Modo de creación', default='manual')
+                                          string='Modo de creación', default='manual')
 
     @api.onchange('start_date')
     def onchange_start_date(self):
