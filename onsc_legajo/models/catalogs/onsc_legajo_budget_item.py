1af0799fce5cba8b3bb8153400073321df681986
raguero </SUlem@2017/>
2023-02-03 13:16:02 -0300

    [ADD] LEG_1.30_02 Integración WS2 CGN -Integración WS2 CGN

--- onsc_legajo/models/catalogs/onsc_legajo_budget_item.py
+++ onsc_legajo/models/catalogs/onsc_legajo_budget_item.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from odoo import fields, models, api, tools
+from odoo import fields, models, api, tools, _
+from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
 
 
 class ONSCLegajoBudgetItem(models.Model):
     _name = 'onsc.legajo.budget.item'
+    _inherit = 'onsc.legajo.abstract.sync'
     _description = 'Partida'
 
     codPartida = fields.Char(string="Código de partida", required=True)
@@ -25,7 +27,7 @@ class ONSCLegajoBudgetItem(models.Model):
     def syncronize(self):
         parameter = self.env['ir.config_parameter'].sudo().get_param('onsc_legajo_WS2_partidas')
         cron = self.env.ref("onsc_legajo.sync_legajo_budget_item")
-        integration_error = self.env.ref("onsc_legajo.onsc_legajo_integration_error_WS3_9005")
+        integration_error = self.env.ref("onsc_legajo.onsc_legajo_integration_error_WS2_9005")
         return self._syncronize(parameter, cron.name, integration_error)
 
     def _populate_from_syncronization(self, response):
@@ -39,25 +41,25 @@ class ONSCLegajoBudgetItem(models.Model):
         integration_error_WS2_9001 = self.env.ref("onsc_legajo.onsc_legajo_integration_error_WS2_9001")
         integration_error_WS2_9002 = self.env.ref("onsc_legajo.onsc_legajo_integration_error_WS2_9002")
 
-        for external_record in response.listaRegimenPlaza:
+        for external_record in response.listaPartidas:
             key_str = str(external_record.codPartida)
             all_external_ley_list.append(key_str)
 
-            vals = self._prepare_values(external_record)
+            vals = self._prepare_values(external_record, cron)
             # CREANDO NUEVO ELEMENTO
             if key_str not in all_odoo_recordsets_key_list:
                 try:
-                    vals['codPartida'] = key_str
-                    self.create(vals)
-                    self._create_log(
-                        origin=cron.name,
-                        type='info',
-                        integration_log=integration_error_WS2_9000,
-                        ws_tuple=external_record,
-                        long_description='Evento: Creación'
-                    )
+                    with self._cr.savepoint():
+                        vals['codPartida'] = key_str
+                        self.create(vals)
+                        self._create_log(
+                            origin=cron.name,
+                            type='info',
+                            integration_log=integration_error_WS2_9000,
+                            ws_tuple=external_record,
+                            long_description='Evento: Creación'
+                        )
                 except Exception as e:
-                    self.env.cr.commit()
                     _logger.warning(tools.ustr(e))
                     self._create_log(
                         origin=cron.name,
@@ -68,16 +70,16 @@ class ONSCLegajoBudgetItem(models.Model):
             # MODIFICANDO ELEMENTO EXISTENTE
             else:
                 try:
-                    all_odoo_recordsets.filtered(lambda x: x.codPartida == key_str).write(vals)
-                    self._create_log(
-                        origin=cron.name,
-                        type='info',
-                        integration_log=integration_error_WS2_9000,
-                        ws_tuple=external_record,
-                        long_description='Evento: Actualización'
-                    )
+                    with self._cr.savepoint():
+                        all_odoo_recordsets.filtered(lambda x: x.codPartida == key_str).write(vals)
+                        self._create_log(
+                            origin=cron.name,
+                            type='info',
+                            integration_log=integration_error_WS2_9000,
+                            ws_tuple=external_record,
+                            long_description='Evento: Actualización'
+                        )
                 except Exception as e:
-                    self.env.cr.commit()
                     _logger.warning(tools.ustr(e))
                     self._create_log(
                         origin=cron.name,
@@ -90,32 +92,61 @@ class ONSCLegajoBudgetItem(models.Model):
             'active': False
         })
 
-    def _prepare_values(self, external_record):
-        Descriptor1 = self.env['onsc.catalog.descriptor1']
-        Descriptor2 = self.env['onsc.catalog.descriptor2']
-        Descriptor3 = self.env['onsc.catalog.descriptor3']
-        Descriptor4 = self.env['onsc.catalog.descriptor4']
-        vals = {'active': True}
-        if external_record.dsc1Id:
-            vals['dsc1Id'] = Descriptor1.search([('code', '=', external_record.dsc1Id)], limit=1).id
-        if external_record.dsc2Id:
-            vals['dsc2Id'] = Descriptor2.search([('code', '=', external_record.dsc2Id)], limit=1).id
-        if external_record.dsc3Id:
-            descriptor3 = Descriptor3.search([('code', '=', external_record.dsc3Id)], limit=1)
-            if descriptor3:
-                vals['desc3Id'] = descriptor3.id
-            else:
-                vals['desc3Id'] = Descriptor3.create({
-                    'code': external_record.dsc3Id,
-                    'name': external_record.Dsc3Descripcion
-                }).id
-        if external_record.dsc4Id:
-            descriptor4 = Descriptor4.search([('code', '=', external_record.dsc4Id)], limit=1)
-            if descriptor4:
-                vals['desc4Id'] = descriptor4.id
-            else:
-                vals['desc4Id'] = Descriptor4.create({
-                    'code': external_record.dsc4Id,
-                    'name': external_record.Dsc4Descripcion
-                }).id
-        return vals
+    def _prepare_values(self, external_record, cron):
+        try:
+            with self._cr.savepoint():
+                Descriptor1 = self.env['onsc.catalog.descriptor1']
+                Descriptor2 = self.env['onsc.catalog.descriptor2']
+                Descriptor3 = self.env['onsc.catalog.descriptor3']
+                Descriptor4 = self.env['onsc.catalog.descriptor4']
+                vals = {
+                    'dsc1Id': False,
+                    'dsc2Id': False,
+                    'dsc3Id': False,
+                    'dsc4Id': False,
+                    'active': True
+                }
+                if external_record.dsc1Id:
+                    descriptor1 = Descriptor1.suspend_security().search([
+                        ('code', '=', external_record.dsc1Id)], limit=1)
+                    if descriptor1 is False:
+                        raise ValidationError(
+                            _('El descriptor 1 con código %s no ha sido identificado') % external_record.dsc1Id)
+                    vals['dsc1Id'] = descriptor1.id
+                if external_record.dsc2Id:
+                    descriptor2 = Descriptor2.suspend_security().search([
+                        ('code', '=', external_record.dsc2Id)], limit=1)
+                    if descriptor2 is False:
+                        raise ValidationError(
+                            _('El descriptor 2 con código %s no ha sido identificado') % external_record.dsc2Id)
+                    vals['dsc2Id'] = descriptor2.id
+                if external_record.dsc3Id:
+                    descriptor3 = Descriptor3.suspend_security().search([
+                        ('code', '=', external_record.dsc3Id)], limit=1)
+                    if descriptor3:
+                        vals['dsc3Id'] = descriptor3.id
+                    else:
+                        vals['dsc3Id'] = Descriptor3.suspend_security().create({
+                            'code': external_record.dsc3Id,
+                            'name': external_record.dsc3Descripcion
+                        }).id
+                if external_record.dsc4Id:
+                    descriptor4 = Descriptor4.suspend_security().search([
+                        ('code', '=', external_record.dsc4Id)], limit=1)
+                    if descriptor4:
+                        vals['dsc4Id'] = descriptor4.id
+                    else:
+                        vals['dsc4Id'] = Descriptor4.suspend_security().create({
+                            'code': external_record.dsc4Id,
+                            'name': external_record.dsc4Descripcion
+                        }).id
+                return vals
+        except Exception as e:
+            _logger.warning(tools.ustr(e))
+            integration_error_WS2_9002 = self.env.ref("onsc_legajo.onsc_legajo_integration_error_WS2_9004")
+            self._create_log(
+                origin=cron.name,
+                type='error',
+                integration_log=integration_error_WS2_9002,
+                ws_tuple=external_record,
+                long_description=tools.ustr(e))
