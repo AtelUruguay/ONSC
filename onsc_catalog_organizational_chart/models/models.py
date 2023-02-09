7634f0c8e70b5b4554cafb02081a0b9ca03c5fc9
raguero </SUlem@2017/>
2023-02-09 12:23:39 -0300

    [ADD] Mejoras a histórico

--- onsc_catalog_organizational_chart/models/models.py
+++ onsc_catalog_organizational_chart/models/models.py
@@ -9,3 +9,14 @@ class Department(models.Model):
     show_short_name = fields.Boolean(
         'Nombre corto en organigrama'
     )
+
+    history_name = fields.Char(compute = '_compute_history_name')
+
+    def _compute_history_name(self):
+        as_of_date = self._context.get('as_of_date', fields.Date.today())
+        for record in self:
+            record_withhistory = record.with_context(find_history=True, as_of_date=as_of_date)
+            if record_withhistory:
+                record.history_name = record_withhistory.name_get()[0][1]
+            else:
+                record.history_name = self.inciso_id.name
