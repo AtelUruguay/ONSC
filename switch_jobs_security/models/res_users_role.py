e9c6db864a9221e5bd9dcf346b8cab3108577a9c
raguero </SUlem@2017/>
2023-03-02 12:49:45 -0300

    [ADD] PS07 LEG_9.1_01 Ajustes a la aplicación (Seguridad por puesto)
    -Versión con roles por defecto en el Default User

--- switch_jobs_security/models/res_users_role.py
+++ switch_jobs_security/models/res_users_role.py
@@ -6,7 +6,7 @@ from odoo import fields, models
 class ResUsersRole(models.Model):
     _inherit = "res.users.role"
 
-    is_config_onsc_role = fields.Boolean(string='Habilitado por inciso')
+    is_byinciso = fields.Boolean(string='Habilitado por inciso')
 
 
 class ResUsersRole(models.Model):
