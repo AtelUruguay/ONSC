b8bbf5b9c8a1b95a445c94001e549547d98deff5
david <davidmonterocrespo24@gmail.com>
2023-03-06 15:10:42 -0300

    [FIX][onsc_legajo]Correcciones de seguridad y vistas del contrato
    Signed-off-by: david <davidmonterocrespo24@gmail.com>

--- onsc_legajo/models/hr_contract.py
+++ onsc_legajo/models/hr_contract.py
@@ -14,8 +14,9 @@ class HrContract(models.Model):
 
     income_mechanism_id = fields.Many2one('onsc.legajo.income.mechanism', string='Mecanismo de ingreso')
     call_number = fields.Char(string='Número de llamado')
-    legajo_state = fields.Selection([('active', 'Activo'), ('baja', 'Baja'), ('outgoing_commission', 'Comisión saliente'),
-                              ('incoming_commission', 'Comisión entrante')], string='Estado')
+    legajo_state = fields.Selection(
+        [('active', 'Activo'), ('baja', 'Baja'), ('outgoing_commission', 'Comisión saliente'),
+         ('incoming_commission', 'Comisión entrante')], string='Estado')
     cs_contract_id = fields.Many2one('hr.contract', string='Contrato cs')
     first_name = fields.Char(string=u'Primer nombre', related='employee_id.cv_first_name')
     second_name = fields.Char(string=u'Segundo nombre', related='employee_id.cv_second_name')
@@ -25,7 +26,7 @@ class HrContract(models.Model):
                                          related='employee_id.cv_emissor_country_id')
     document_type_id = fields.Many2one('onsc.cv.document.type', string=u'Tipo de documento',
                                        related='employee_id.cv_document_type_id')
-    nro_doc = fields.Char(u'Número de documento', history=True, related='employee_id.cv_nro_doc')
+    nro_doc = fields.Char(u'Número de documento', related='employee_id.cv_nro_doc')
     program = fields.Char(string='Programa')
     project = fields.Char(string='Proyecto')
     regime_id = fields.Many2one('onsc.legajo.regime', string='Régimen')
@@ -39,9 +40,11 @@ class HrContract(models.Model):
     workplace = fields.Char(string='Plaza')
     reason_discharge = fields.Char(string='Descripción del motivo alta')
     norm_code_discharge_id = fields.Many2one('onsc.legajo.norm', string='Código de norma alta')
-    norm_number_discharge = fields.Char(string='Número de norma alta')
-    norm_year_discharge = fields.Char(string='Año de norma alta')
-    norm_article_discharge = fields.Char(string='Artículo de norma alta')
+    norm_number_discharge = fields.Integer(string='Número de norma alta',
+                                           related='norm_code_discharge_id.numeroNorma')
+    norm_year_discharge = fields.Integer(string='Año de norma alta', related='norm_code_discharge_id.anioNorma')
+    norm_article_discharge = fields.Integer(string='Artículo de norma alta',
+                                            related='norm_code_discharge_id.articuloNorma')
     resolution_description_discharge = fields.Char(string='Descripción de la resolución')
     resolution_date_discharge = fields.Date(string='Fecha de la resolución')
     resolution_type_discharge = fields.Selection(
@@ -53,13 +56,12 @@ class HrContract(models.Model):
     description_day = fields.Char(string="Descripción de la Jornada")
     retributive_day_id = fields.Many2one('onsc.legajo.jornada.retributiva', string='Jornada retributiva')
     id_registration_discharge = fields.Char(string='Id de alta')
-    discharge_date = fields.Date(string='Fecha de baja')
     id_deregistration_discharge = fields.Char(string='Id de baja')
     reason_deregistration = fields.Char(string='Descripción del motivo baja')
     norm_code_deregistration_id = fields.Many2one('onsc.legajo.norm', string='Código de la norma de baja')
-    norm_number_deregistration = fields.Char(string='Número de norma baja')
-    norm_year_deregistration = fields.Char(string='Año de norma baja')
-    norm_article_deregistration = fields.Char(string='Artículo de norma baja')
+    norm_number_deregistration = fields.Integer(string='Número de norma baja',related='norm_code_deregistration_id.numeroNorma')
+    norm_year_deregistration = fields.Integer(string='Año de norma baja',related='norm_code_deregistration_id.anioNorma')
+    norm_article_deregistration = fields.Integer(string='Artículo de norma baja',related='norm_code_deregistration_id.articuloNorma')
     resolution_description_deregistration = fields.Char(string='Descripción de la resolución baja')
     resolution_date_deregistration = fields.Date(string='Fecha de la resolución baja')
     resolution_type_deregistration = fields.Selection(
@@ -67,6 +69,7 @@ class HrContract(models.Model):
         string='Tipo de resolución baja')
     causes_discharge_id = fields.Many2one("onsc.legajo.causes.discharge", string="Causal de egreso")
     causes_discharge_extended = fields.Char(string='Causal de egreso extendida')
+    code_cgn = fields.Char(string='Código CGN', related='causes_discharge_id.code_cgn')
     additional_information_deregistration = fields.Char(string='Información adicional baja')
     attached_document_discharge_ids = fields.One2many('onsc.legajo.attached.document',
                                                       'contract_id',
@@ -86,8 +89,10 @@ class HrContract(models.Model):
     def onchange_employee(self):
         if self.employee_id:
             name = self.employee_id.cv_nro_doc if self.employee_id.cv_nro_doc else ''
+            if self.employee_id.name and self.sec_position:
+                name += ' - '
             if self.sec_position:
-                name += ' - ' + self.sec_position if self.sec_position else ''
+                name += self.sec_position if self.sec_position else ''
             self.name = name
         else:
             self.name = False
