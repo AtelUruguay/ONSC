77439e686c64bcfaa0a12fd37865fa1c7ced89c0
david <davidmonterocrespo24@gmail.com>
2023-05-12 18:16:24 -0300

    [FIX][onsc_legajo] Filtrar por los usuarios que tienen el grupo de recursos humanos Inciso y/o UE y con un contrato activo en el mismo inciso y /o UE del alta que se está dando
    Signed-off-by: david <davidmonterocrespo24@gmail.com>

--- onsc_legajo/wizard/mail_wizard_invite.py
+++ onsc_legajo/wizard/mail_wizard_invite.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from lxml import etree
-from lxml.html import builder as html
-
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError
+from odoo import api, fields, models
 
 
 class Invite(models.TransientModel):
@@ -18,13 +14,20 @@ class Invite(models.TransientModel):
         res_id = context.get('default_res_id', False)
         if model == 'onsc.legajo.alta.vl' and res_id:
             alta_id = self.env[model].browse(res_id)
-            partner_ids = self.env['hr.contract'].search(
-                [
-                    ('operating_unit_id', '=', alta_id.operating_unit_id.id)
-                ]).mapped('employee_id.user_id.partner_id')
+            user_ids = self.env['hr.contract'].search(
+                [('legajo_state', '=', 'active'),
+                 ('operating_unit_id', '=', alta_id.operating_unit_id.id)
+                 ]).mapped('employee_id.user_id')
+
+            recursos_humanos_inciso = self.env.ref('onsc_legajo.group_legajo_alta_vl_recursos_humanos_inciso')
+            recursos_humanos_ue = self.env.ref('onsc_legajo.group_legajo_alta_vl_recursos_humanos_ue')
+            user_ids = user_ids.filtered(
+                lambda u: recursos_humanos_inciso in u.groups_id or recursos_humanos_ue in u.groups_id)
+            partner_ids = user_ids.mapped('partner_id')
+
             return [('id', 'in', partner_ids.ids)]
         return [('type', '!=', 'private')]
 
-    partner_ids = fields.Many2many('res.partner', string='Recipients', help="List of partners that will be added as follower of the current document.",
+    partner_ids = fields.Many2many('res.partner', string='Recipients',
+                                   help="List of partners that will be added as follower of the current document.",
                                    domain=_get_domain_partner_ids)
-
