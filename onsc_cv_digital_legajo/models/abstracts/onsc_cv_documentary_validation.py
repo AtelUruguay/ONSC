42506da6cef54e48cab7b18ae23f7b03821a98fb
raguero </SUlem@2017/>
2023-06-01 11:38:20 -0300

    [FIX] Mejoras varias Sprint 7

--- onsc_cv_digital_legajo/models/abstracts/onsc_cv_documentary_validation.py
+++ onsc_cv_digital_legajo/models/abstracts/onsc_cv_documentary_validation.py
@@ -17,6 +17,39 @@ class ONSCCVAbstractFileValidation(models.AbstractModel):
 
     @property
     def widget_call_documentary_button(self):
+        if self._context.get('is_legajo'):
+            return etree.XML("""
+                            <div>
+                                <field name="documentary_validation_state" invisible="1"/>
+                                <button name="button_documentary_approve"
+                                    attrs="{'invisible': [('documentary_validation_state', '=', 'validated')]}"
+                                    groups="onsc_cv_digital.group_validador_documental_cv,onsc_cv_digital_legajo.group_legajo_validador_doc_inciso,onsc_cv_digital_legajo.group_legajo_validador_doc_ue"
+                                    type="object" string="Validar" icon="fa-thumbs-o-up" class="btn btn-sm btn-outline-success"/>
+                                <button name="button_documentary_reject"
+                                    attrs="{'invisible': [('documentary_validation_state', '=', 'rejected')]}"
+                                    groups="onsc_cv_digital.group_validador_documental_cv,onsc_cv_digital_legajo.group_legajo_validador_doc_inciso,onsc_cv_digital_legajo.group_legajo_validador_doc_ue"
+                                    type="object" string="Rechazar" icon="fa-thumbs-o-down" class="btn btn-sm btn-outline-danger"/>
+                                <div class="alert alert-danger" role="alert"
+                                    attrs="{'invisible': [('documentary_validation_state', '!=', 'rejected')]}">
+                                    <p class="mb-0">
+                                        <strong>
+                                            El registro ha sido rechazado. Motivo del rechazo: <field name="documentary_reject_reason" class="oe_inline" readonly="1"/>
+                                            <p/>
+                                            Fecha: <field name="documentary_validation_date" class="oe_inline" readonly="1"/> Usuario: <field name="documentary_user_id" class="oe_inline" options="{'no_open': True, 'no_quick_create': True, 'no_create': True}" readonly="1"/>
+                                        </strong>
+                                    </p>
+                                </div>
+                                <div class="alert alert-success" role="alert"
+                                    attrs="{'invisible': [('documentary_validation_state', '!=', 'validated')]}">
+                                    <p class="mb-0">
+                                        <strong>
+                                            El registro ha sido validado
+                                            <p/>
+                                            Fecha: <field name="documentary_validation_date" class="oe_inline" readonly="1"/> Usuario: <field name="documentary_user_id" class="oe_inline" options="{'no_open': True, 'no_quick_create': True, 'no_create': True}" readonly="1"/>
+                                        </strong>
+                                    </p>
+                                </div>
+                            </div>""")
         return etree.XML("""
                 <div>
                     <field name="documentary_validation_state" invisible="1"/>
@@ -103,16 +136,22 @@ class ONSCCVAbstractFileValidation(models.AbstractModel):
             Calls = self.env['onsc.cv.digital.call']
             for record in self.filtered(lambda x: x.original_instance_identifier == 0 and x.cv_digital_id.type == 'cv'):
                 # OBTENIENDO LLAMADOS DEL CV
-                calls = Calls.search([
+                calls = Calls.with_context(unactive_user_config=True).search([
                     ('cv_digital_origin_id', '=', record.cv_digital_id.id),
                     ('is_zip', '=', False),
                     ('preselected', '!=', 'no'),
                 ])
                 # OBTENIENDO RECORDSETS ASOCIADOS A LLAMADOS Y QUE TENGAN COMO ORIGEN EL RECORD
-                self.search([
+                recordsets = self.search([
                     ('original_instance_identifier', '=', record.id),
                     ('cv_digital_id', 'in', calls.mapped('cv_digital_id').ids),
-                    ('create_date', '>=', record.custom_write_date)]).button_documentary_approve()
+                    ('create_date', '>=', record.custom_write_date)])
+                if args.get('documentary_validation_state') == 'rejected':
+                    recordsets.documentary_reject(args.get('documentary_reject_reason'))
+                elif args.get('documentary_validation_state') == 'to_validate':
+                    recordsets.button_documentary_tovalidate()
+                else:
+                    recordsets.button_documentary_approve()
                 calls.button_update_documentary_validation_sections_tovalidate()
             self._update_call_documentary_validation_status()
 
