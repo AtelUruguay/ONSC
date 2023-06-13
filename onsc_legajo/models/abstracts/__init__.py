9a0d58db8423436e2e7034e97f64a4dbd88484ee
david <davidmonterocrespo24@gmail.com>
2023-06-13 19:35:29 -0300

    [FIX][onsc_legajo] CI/CD

--- onsc_legajo/models/abstracts/__init__.py
+++ onsc_legajo/models/abstracts/__init__.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from . import onsc_legajo_abstract_sync
+from . import onsc_legajo_abstract_legajo_security
+from . import onsc_abstract_actions_common_data
+from . import onsc_legajo_abstract_alta_vl_ws1
+from . import onsc_legajo_abstract_alta_vl_ws4
+from . import onsc_legajo_abstract_baja_vl_ws9
+from . import onsc_legajo_abstract_ws12
+from . import onsc_legajo_abstract_alta_cs_ws10
