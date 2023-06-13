332d6a3fa667495a9b63046a8d01d00734c72c31
raguero <raguero@quanam.com>
2023-06-13 12:46:06 -0300

    [HOTFIX][CV DIGITAL][PS07 11221,11176,11217,11216] Liberación 11.4

--- onsc_cv_digital/models/onsc_cv_digital.py
+++ onsc_cv_digital/models/onsc_cv_digital.py
@@ -862,7 +862,7 @@ class ONSCCVDigital(models.Model):
             else:
                 show_disabilitie = False
             call_doc_val_afro_show = (is_call_documentary_validation and (
-                        cv_call_id.is_cv_race_public or cv_call_id.is_afro) and cv_call_id.is_afrodescendants)
+                        cv_call_id.is_cv_race_public or cv_call_id.is_afro) and cv_call_id.is_afro_descendants)
             if cv_call_id.is_cv_race_public or is_cv_copy or call_doc_val_afro_show:
                 show_afro = True
             else:
