336024f8af15210f59fd8cdefc37857d86fb1fd0
raguero <raguero@quanam.com>
2023-08-08 14:42:31 -0300

    [ADD][DES] GDD_2.2.1.01 Generar lista de participantes

--- onsc_desempeno/models/onsc_desempeno_evaluation_list.py
+++ onsc_desempeno/models/onsc_desempeno_evaluation_list.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
+import logging
 from collections import defaultdict
 
-import logging
-
 from odoo import fields, models, api
-
+from odoo.osv import expression
 
 _logger = logging.getLogger(__name__)
 
@@ -13,6 +12,37 @@ class ONSCDesempenoEvaluationList(models.Model):
     _name = 'onsc.desempeno.evaluation.list'
     _description = u'Lista de participantes de evaluaciones 360°'
 
+    def _get_domain(self, args):
+        if self.user_has_groups('onsc_desempeno.group_desempeno_usuario_gh_inciso'):
+            inciso_id = self.env.user.employee_id.job_id.contract_id.inciso_id.id
+            args = expression.AND([[('inciso_id', '=', inciso_id), ], args])
+        elif self.user_has_groups('onsc_desempeno.group_desempeno_usuario_gh_ue'):
+            operating_unit_id = self.env.user.employee_id.job_id.contract_id.operating_unit_id.id
+            args = expression.AND([[('operating_unit_id', '=', operating_unit_id), ], args])
+        elif self.user_has_groups('onsc_desempeno.group_desempeno_responsable_uo'):
+            department_id = self.env.user.employee_id.job_id.department_id.id
+            department_ids = self.env['hr.department'].search(['|',
+                                                               ('id', 'child_of', department_id),
+                                                               ('id', '=', department_id)]).ids
+            args = expression.AND([[('department_id', 'in', department_ids), ], args])
+
+        return args
+
+    @api.model
+    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+        if self._context.get('is_from_menu'):
+            args = self._get_domain(args)
+        return super(ONSCDesempenoEvaluationList, self)._search(args, offset=offset, limit=limit, order=order,
+                                                                count=count,
+                                                                access_rights_uid=access_rights_uid)
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+        if self._context.get('is_from_menu'):
+            domain = self._get_domain(domain)
+        return super().read_group(domain, fields, groupby, offset=offset, limit=limit, orderby=orderby, lazy=lazy)
+
+    name = fields.Char(string='Nombre', compute='_compute_name', store=True)
     evaluation_stage_id = fields.Many2one(
         'onsc.desempeno.evaluation.stage',
         string=u'Evaluación 360',
@@ -30,6 +60,11 @@ class ONSCDesempenoEvaluationList(models.Model):
         string=u'Fecha fin def. entorno',
         related='evaluation_stage_id.end_date_environment',
         store=True)
+    inciso_id = fields.Many2one(
+        "onsc.catalog.inciso",
+        string="Inciso",
+        related="evaluation_stage_id.inciso_id",
+        store=True)
     operating_unit_id = fields.Many2one(
         "operating.unit",
         string="Unidad ejecutora",
@@ -40,7 +75,11 @@ class ONSCDesempenoEvaluationList(models.Model):
         string="Unidad organizativa",
         required=True,
         index=True)
-    manager_id = fields.Many2one("hr.employee", string="Líder", required=True, index=True)
+    manager_id = fields.Many2one(
+        "hr.employee",
+        string="Líder",
+        compute='_compute_manager_id',
+        store=False)
     state = fields.Selection(
         string='Formulario',
         selection=[('in_progress', 'En proceso'),
@@ -53,6 +92,97 @@ class ONSCDesempenoEvaluationList(models.Model):
         inverse_name='evaluation_list_id',
         string='Colaboradores')
 
+    should_disable_form_edit = fields.Boolean(string="Deshabilitar botón de editar",
+                                              compute='_compute_should_disable_form_edit')
+
+    _sql_constraints = [
+        ('recordset_uniq', 'unique(department_id,evaluation_stage_id)',
+         u'Ya existe una lista de evaluación para esta unidad organizativa y ciclo de evaluación.'),
+    ]
+
+    @api.depends('evaluation_stage_id', 'manager_id')
+    def _compute_name(self):
+        for rec in self:
+            rec.name = 'Evaluación: %s, Responsable: %s' % (
+                rec.evaluation_stage_id.name,
+                rec.manager_id.name)
+
+    def _compute_manager_id(self):
+        for rec in self:
+            rec.manager_id = rec.department_id.get_first_department_withmanager_in_tree().manager_id.id
+
+    @api.depends('end_date', 'state')
+    def _compute_should_disable_form_edit(self):
+        for record in self:
+            record.should_disable_form_edit = record.state == 'closed' or record.end_date < fields.Date.today()
+
+    def button_generate_forms(self):
+        return True
+
+    # INTELIGENCIA
+    def manage_evaluations_lists(self):
+        # cerrar las listas que ya pasaron la fecha de cierre
+        self.search([('end_date', '<', fields.Date.today())]).write({'state': 'closed'})
+
+        evaluation_stages = self.env['onsc.desempeno.evaluation.stage'].search([
+            ('start_date', '<=', fields.Date.today()),
+            ('end_date', '>=', fields.Date.today()),
+        ])
+        inlist_evaluation_stage_ids = self.search(
+            [('evaluation_stage_id', 'in', evaluation_stages.ids)]).mapped('evaluation_stage_id.id')
+        # si ya esta la lista creada para esa UE excluir
+        for evaluation_stage in evaluation_stages.filtered(lambda x: x.id not in inlist_evaluation_stage_ids):
+            self._create_data(evaluation_stage)
+        return True
+
+    def _create_data(self, evaluation_stage):
+        Jobs = self.env['hr.job'].suspend_security()
+        EvaluationList = self.env['onsc.desempeno.evaluation.list']
+        evaluation_lists = self.env['onsc.desempeno.evaluation.list']
+
+        exluded_descriptor1_ids = self.env.company.descriptor1_ids.ids
+        jobs = Jobs.search([
+            ('department_id.hierarchical_level_id.order', '!=', 1),
+            ('department_id.operating_unit_id', '=', evaluation_stage.operating_unit_id.id),
+            ('contract_id.legajo_state', 'in', ['active', 'incoming_commission']),
+            ('contract_id.descriptor1_id', 'not in', exluded_descriptor1_ids),
+            '|',
+            ('end_date', '>=', evaluation_stage.start_date),
+            ('end_date', '=', False),
+        ])
+        # Creamos un diccionario con defaultdict para evitar comprobaciones de existencia
+        departments_grouped_info = defaultdict(lambda: {'job_ids': set()})
+        for job in jobs:
+            # manager = job.department_id.get_first_department_withmanager_in_tree().manager_id
+            # if manager:
+            departments_grouped_info[job.department_id]['department_id'] = job.department_id
+            # departments_grouped_info[job.department_id]['manager_id'] = manager
+            departments_grouped_info[job.department_id]['job_ids'].add(job)
+
+        # Convertimos el diccionario de defaultdict a un diccionario estándar
+        departments_grouped_info = dict(departments_grouped_info)
+        for department, info in departments_grouped_info.items():
+            evaluation_vals = {
+                'evaluation_stage_id': evaluation_stage.id,
+                'department_id': department.id,
+                # 'manager_id': info.get('manager_id', self.env['hr.employee']).id,
+            }
+            line_vals = []
+            for job in info.get('job_ids', []):
+                line_vals.append([0, 0, {
+                    'job_id': job.id,
+                }])
+            evaluation_vals['line_ids'] = line_vals
+            evaluation_lists |= EvaluationList.create(evaluation_vals)
+        return evaluation_lists
+
+    def _link_jobs(self, jobs):
+        for job in jobs:
+            self.search([
+                ('department_id', '=', job.department_id.id),
+                ('state', '=', 'in_progress')]).write({'line_ids': [(0, 0, {'job_id': job.id})]})
+        return True
+
 
 class ONSCDesempenoEvaluationListLine(models.Model):
     _name = 'onsc.desempeno.evaluation.list.line'
@@ -61,6 +191,7 @@ class ONSCDesempenoEvaluationListLine(models.Model):
     evaluation_list_id = fields.Many2one(
         comodel_name='onsc.desempeno.evaluation.list',
         string='Evaluation_list_id',
+        ondelete='cascade',
         required=True)
     job_id = fields.Many2one(
         "hr.job",
@@ -97,9 +228,7 @@ class ONSCDesempenoEvaluationListLine(models.Model):
         store=True)
 
     is_included = fields.Boolean(string='Incluir en el ciclo', default=True)
-    excluded_cause = fields.Char(
-        string='Motivo exclusión',
-        required=False)
+    excluded_cause = fields.Text(string='Motivo exclusión')
     state = fields.Selection(
         string='Formulario',
         selection=[('stand', 'Pendiente'),
@@ -107,6 +236,15 @@ class ONSCDesempenoEvaluationListLine(models.Model):
                    ('generated', 'Generado')],
         required=True,
         default='stand')
+    active = fields.Boolean(
+        string='',
+        compute='_compute_active',
+        store=True
+    )
+
+    @api.onchange('is_included')
+    def onchange_is_included(self):
+        self.excluded_cause = ''
 
     @api.depends('employee_id')
     def _compute_legajo_id(self):
@@ -114,57 +252,15 @@ class ONSCDesempenoEvaluationListLine(models.Model):
         for record in self:
             record.legajo_id = Legajo.search([('employee_id', '=', record.employee_id.id)], limit=1)
 
-    # INTELIGENCIA
-    def set_evaluation_list(self):
-        evaluation_stages = self.env['onsc.desempeno.evaluation.stage'].search([
-            ('start_date', '<=', fields.Date.today()),
-            ('end_date', '>=', fields.Date.today()),
-        ])
-        inlist_evaluation_stage_ids = self.search(
-            [('evaluation_stage_id', 'in', evaluation_stages.ids)].mapped('evaluation_stage_id.id'))
-        # si ya esta la lista creada para esa UE excluir
-        for evaluation_stage in evaluation_stages.filtered(lambda x: x.id not in inlist_evaluation_stage_ids):
-            evaluation_lists = self._create_data(evaluation_stage)
-            a = 5
+    @api.depends('job_id.end_date', 'state')
+    def _compute_active(self):
+        for record in self:
+            job_active = record.job_id.end_date is False or record.job_id.end_date >= fields.Date.today()
+            record.active = job_active and record.state != 'generated'
 
-
-    def _create_data(self, evaluation_stage):
-        Jobs = self.env['hr.job'].suspend_security()
-        EvaluationList = self.env['onsc.desempeno.evaluation.list']
-        evaluation_lists = self.env['onsc.desempeno.evaluation.list']
-
-        exluded_descriptor1_ids = self.env.company.descriptor1_ids.ids
-        jobs = Jobs.search([
-            ('department_id.hierarchical_level_id.order', '=', 1),
-            ('department_id.operating_unit_id', '=', evaluation_stage.operating_unit_id.id),
-            ('contract_id.legajo_state', 'in', ['active', 'incoming_commission']),
-            ('descriptor1_id', 'not in', exluded_descriptor1_ids),
-            '|',
-            ('end_date', '>=', evaluation_stage.start_date),
-            ('end_date', '=', False),
-        ])
-        # Creamos un diccionario con defaultdict para evitar comprobaciones de existencia
-        departments_grouped_info = defaultdict(lambda: {'job_ids': set()})
-        for job in jobs:
-            manager = job.department_id.get_first_department_withmanager_in_tree().manager_id
-            departments_grouped_info[job.department_id]['department_id'] = job.department_id
-            departments_grouped_info[job.department_id]['manager_id'] = manager
-            departments_grouped_info[job.department_id]['job_ids'].add(job)
-
-        # Convertimos el diccionario de defaultdict a un diccionario estándar
-        departments_grouped_info = dict(departments_grouped_info)
-        for department, info in departments_grouped_info.items():
-            evaluation_vals = [0, 0, {
-                'evaluation_stage_id': evaluation_stage.id,
-                'department_id': department.id,
-                'manager_id': info.get('manager_id', self.env['hr.employee']).id,
-            }]
-            line_vals = []
-            for job in info.get('job_ids', []):
-                line_vals.append([0, 0, {
-                    'job_id': job.id,
-                }])
-            evaluation_vals[2]['line_ids'] = line_vals
-            evaluation_lists |= EvaluationList.create(evaluation_vals)
-        return evaluation_lists
-
+    def button_open_current_contract(self):
+        action = self.sudo().env.ref('onsc_legajo.onsc_legajo_one_hr_contract_action').read()[0]
+        action.update({'res_id': self.contract_id.id})
+        ctx = self.env.context.copy()
+        ctx.update({'edit': False})
+        return action
