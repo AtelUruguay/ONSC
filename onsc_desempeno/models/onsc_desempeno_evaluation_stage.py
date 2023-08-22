0955cf6ee09b6d576ebe3bfa9375a35115e6475a
raguero <raguero@quanam.com>
2023-08-22 00:50:39 -0300

    [ADD][DESEMPENO] Mejoras varias [ADD][LEGAJO] DF Migración y WS7

--- onsc_desempeno/models/onsc_desempeno_evaluation_stage.py
+++ onsc_desempeno/models/onsc_desempeno_evaluation_stage.py
@@ -170,10 +170,10 @@ class ONSCDesempenoEvaluationStage(models.Model):
         for record in self:
             if record.start_date < record.general_cycle_id.start_date:
                 raise ValidationError(
-                    _(u"La fecha inicio de las Etapas de evaluaciones 360° por UE de debe ser mayor o igual a la fecha de inicio del Ciclo General"))
+                    _(u"La fecha inicio de las Etapas de evaluaciones 360° por UE debe ser mayor o igual a la fecha de inicio del Ciclo General"))
             if record.start_date > record.general_cycle_id.start_date_max:
                 raise ValidationError(
-                    _(u"La fecha inicio de las Etapas de evaluaciones 360° por UE de debe ser menor o igual a la fecha de inicio máxima del Ciclo General"))
+                    _(u"La fecha inicio de las Etapas de evaluaciones 360° por UE debe ser menor o igual a la fecha de inicio máxima del Ciclo General"))
 
     @api.constrains("end_date", "general_cycle_id.end_date_max")
     def _check_end_dates(self):
