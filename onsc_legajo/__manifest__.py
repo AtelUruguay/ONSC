a17eeb5b1d015fb9ba9dc98232eaee7f974ab5aa
Rafael Aguero Baquero <raguero@quanam.com>
2023-10-02 08:23:05 -0300

    [LIBERACION]

--- onsc_legajo/__manifest__.py
+++ onsc_legajo/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ONSC - Legajo',
-    'version': '15.0.10.0.0',
+    'version': '15.0.11.0.0',
     'summary': 'ONSC - Legajo',
     'sequence': 11,
     'description': """
