90da3d39824085dc37c4009b0ddc989f1e00bf59
Rafael Aguero Baquero <raguero@quanam.com>
2023-10-27 12:07:43 -0300

    ci/cd

--- onsc_desempeno/models/onsc_desempeno_evaluation.py
+++ onsc_desempeno/models/onsc_desempeno_evaluation.py
@@ -292,9 +292,9 @@ class ONSCDesempenoEvaluation(models.Model):
             is_user_gh_ue_cond = is_gh_user_ue and is_order_1 and same_operating_unit
             is_user_gh_inc_cond = is_gh_user_inciso and is_order_1 and same_inciso
             is_responsable = is_gh_responsable and record.uo_id.id in hierarchy_deparments.ids
+            base_condition = (is_user_gh_ue_cond or is_user_gh_inc_cond or is_responsable)
 
-            record.is_evaluation_change_available = (is_user_gh_ue_cond or is_user_gh_inc_cond or is_responsable) and \
-                                                    is_leader_eval and not is_am_evaluator
+            record.is_evaluation_change_available = base_condition and is_leader_eval and not is_am_evaluator
 
     def button_start_evaluation(self):
         self.write({'state': 'in_process'})
