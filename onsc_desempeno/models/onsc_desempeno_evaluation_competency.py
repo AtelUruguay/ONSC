3d46cf120bd956ad953d27490717103dd8c3bdce
svillanueva <svillanueva@quanam.com>
2023-11-01 17:59:18 -0300

    [Add][Desempeño]GDD_2.8.01 Cierre de Etapa 1 y Generación del Consolidado GDD_2.8.02 Crear el Consolidado GDD_2.8.03 Proceso para Mostrar Respuestas de los Consolidados de Forma Aleatoria

--- onsc_desempeno/models/onsc_desempeno_evaluation_competency.py
+++ onsc_desempeno/models/onsc_desempeno_evaluation_competency.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 _logger = logging.getLogger(__name__)
 
@@ -50,7 +50,7 @@ class ONSCDesempenoEvaluationCompetency(models.Model):
         if is_default:
             return eval("_html2construct")
         for rec in self:
-            _html2construct = HTML_HELP % (rec.skill_id.definition or '')
+            _html2construct = HTML_HELP % (rec.suspend_security().skill_id.definition or '')
             setattr(rec, help_field, _html2construct)
 
     def button_open_current_skill(self):
