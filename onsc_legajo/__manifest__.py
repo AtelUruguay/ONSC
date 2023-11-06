fdd4639445adb0fc717b7f7d500bf198631c8a4d
Rafael Aguero <rafael.aguero@quanam.com>
2023-11-06 15:27:34 -0300

    Update __manifest__.py

--- onsc_legajo/__manifest__.py
+++ onsc_legajo/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ONSC - Legajo',
-    'version': '15.0.12.5.0',
+    'version': '15.0.12.6.0',
     'summary': 'ONSC - Legajo',
     'sequence': 11,
     'description': """
