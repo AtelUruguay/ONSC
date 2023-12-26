a9826b60a7c12532fca6e5eb7e2c189588459d2a
Rafael Aguero Baquero <raguero@quanam.com>
2023-12-26 15:53:56 -0300

    [ADD][DESEMPENO] GDD_5.2.02 Proceso para Generar Puntajes

--- onsc_desempeno/models/onsc_desempeno_score.py
+++ onsc_desempeno/models/onsc_desempeno_score.py
@@ -1,18 +1,43 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.osv import expression
 
 _logger = logging.getLogger(__name__)
-
-SCORE_TYPE = [
-    ('eval_360', 'Evaluación 360'),
-]
 
 
 class ONSCDesempenoScore(models.Model):
     _name = 'onsc.desempeno.score'
     _description = u'Desempeño - Puntajes'
+
+    def _get_domain(self, args):
+        inciso_id = self.env.user.employee_id.job_id.contract_id.inciso_id.id
+        operating_unit_id = self.env.user.employee_id.job_id.contract_id.operating_unit_id.id
+
+        if self.user_has_groups('onsc_desempeno.group_desempeno_administrador'):
+            args_extended = [(1, '=', 1)]
+        elif self.user_has_groups('onsc_desempeno.group_desempeno_admin_gh_inciso'):
+            args_extended = [('inciso_id', '=', inciso_id)]
+        elif self.user_has_groups('onsc_desempeno.group_desempeno_admin_gh_ue'):
+            args_extended = [('operating_unit_id', '=', operating_unit_id)]
+        else:
+            args_extended = [('id', 'in', [])]
+        return expression.AND([args_extended, args])
+
+    @api.model
+    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+        if self._context.get('is_from_menu') and self._context.get('ignore_security_rules', False) is False:
+            args = self._get_domain(args)
+        return super(ONSCDesempenoScore, self)._search(args, offset=offset, limit=limit, order=order,
+                                                       count=count,
+                                                       access_rights_uid=access_rights_uid)
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+        if self._context.get('is_from_menu') and self._context.get('ignore_security_rules', False) is False:
+            domain = self._get_domain(domain)
+        return super().read_group(domain, fields, groupby, offset=offset, limit=limit, orderby=orderby, lazy=lazy)
 
     evaluation_stage_id = fields.Many2one(
         'onsc.desempeno.evaluation.stage',
@@ -20,6 +45,14 @@ class ONSCDesempenoScore(models.Model):
     evaluation_list_id = fields.Many2one(
         comodel_name='onsc.desempeno.evaluation.list',
         string='Lista de participantes')
+    inciso_id = fields.Many2one(
+        'onsc.catalog.inciso',
+        string='Inciso',
+        index=True)
+    operating_unit_id = fields.Many2one(
+        "operating.unit",
+        string="Unidad ejecutora",
+        index=True)
     department_id = fields.Many2one(
         "hr.department",
         string="Unidad organizativa",
@@ -40,3 +73,5 @@ class ONSCDesempenoScore(models.Model):
     evaluations_tracing_plan_finished_score = fields.Float('Puntaje Seguimiento del Plan de desarrollo')
     evaluations_tracing_plan_activity_score = fields.Float('Puntaje de Actividad de Seguimiento del Plan de desarrollo')
     score = fields.Float('Puntaje final')
+
+    is_employee_notified = fields.Boolean(string='¿Fué notificado?')
--- onsc_desempeno/wizard/onsc_user_notification_atlogin.py
+++ onsc_desempeno/wizard/onsc_user_notification_atlogin.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
-import json
 
-from odoo import fields, models, api
+from odoo import fields, models
 
 
 class ONSCDesempenoEvalaluatiorChangeWizard(models.TransientModel):
     _name = 'onsc.user.notification.atlogin'
     _description = 'Mensaje al usuario al login'
 
-    message = fields.Text(string="Mensaje")
+    message = fields.Html(string="Mensaje")
 
     def action_confirm(self):
-        a = 5
+        employee = self.env.user.employee_id
+        year = fields.Date.from_string(fields.Date.today()).year
+        self.env['onsc.desempeno.score'].sudo().search([
+            ('is_employee_notified', '=', False),
+            ('employee_id', '=', employee.id),
+            ('evaluation_stage_id.year', '=', year)
+        ]).write({'is_employee_notified': True})
+
+        action_dict = self.env['res.users'].sudo()._get_default_action().read()[0]
+        self.env.user.sudo().action_id = action_dict.get('id')
+        return action_dict
