4dbf86d07ead6b003a9843d0126b677ad1b0fb90
svillanueva <svillanueva@quanam.com>
2024-01-09 11:36:43 -0300

    [ADD][Desempeño][11668]GDD_8.1.01 Crear Grupo Cuadro resumen

--- onsc_desempeno/models/__init__.py
+++ onsc_desempeno/models/__init__.py
@@ -2,6 +2,7 @@
 from . import res_company
 from . import res_users
 from . import catalogs
+from . import views
 from . import onsc_desempeno_score
 from . import onsc_desempeno_general_cycle
 from . import onsc_desempeno_settings
