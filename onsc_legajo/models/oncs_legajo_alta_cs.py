b211b1fcd92ae669ac7c119a57dd9d6a239b4e56
Rafael Aguero Baquero <raguero@quanam.com>
2024-01-11 10:33:04 -0300

    [FIX][LEGAJO] PS07 12216 Alta de comisión cuando usuario tiene seguridad de GH por UE PS07 12456 Error en producción- Cambio de valor de Fecha Efectiva en alta VL

--- onsc_legajo/models/oncs_legajo_alta_cs.py
+++ onsc_legajo/models/oncs_legajo_alta_cs.py
@@ -60,7 +60,10 @@ class ONSCLegajoAltaCS(models.Model):
         args = expression.AND([[
             ('partner_id', '!=', self.env.user.partner_id.id)
         ], args])
-        if self.user_has_groups('onsc_legajo.group_legajo_hr_inciso_alta_cs'):
+        if self.user_has_groups(
+                'onsc_legajo.group_legajo_alta_cs_administrar_altas_cs,onsc_legajo.group_legajo_consulta_altas_cs'):
+            args = args
+        elif self.user_has_groups('onsc_legajo.group_legajo_hr_inciso_alta_cs'):
             inciso_id = self.env.user.employee_id.job_id.contract_id.inciso_id
             if inciso_id:
                 args = expression.AND([[
@@ -68,7 +71,7 @@ class ONSCLegajoAltaCS(models.Model):
                     '&', ('is_inciso_origin_ac', '=', False), ('inciso_destination_id', '=', inciso_id.id),
                     '&', ('inciso_destination_id', '=', inciso_id.id), ('state', '!=', 'draft')
                 ], args])
-        if self.user_has_groups('onsc_legajo.group_legajo_hr_ue_alta_cs'):
+        elif self.user_has_groups('onsc_legajo.group_legajo_hr_ue_alta_cs'):
             contract_id = self.env.user.employee_id.job_id.contract_id
             inciso_id = contract_id.inciso_id
             operating_unit_id = contract_id.operating_unit_id
@@ -81,7 +84,8 @@ class ONSCLegajoAltaCS(models.Model):
             if operating_unit_id:
                 args = expression.AND([[
                     '|', '|', ('operating_unit_origin_id', '=', operating_unit_id.id),
-                    '&', ('is_inciso_origin_ac', '=', False), ('operating_unit_destination_id', '=', operating_unit_id.id),
+                    '&', ('is_inciso_origin_ac', '=', False),
+                    ('operating_unit_destination_id', '=', operating_unit_id.id),
                     '&', ('operating_unit_destination_id', '=', operating_unit_id.id), ('state', '!=', 'draft')
                 ], args])
         return args
