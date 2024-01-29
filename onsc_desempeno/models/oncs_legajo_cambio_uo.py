bf6fcbf38f439835ea09dc2f5e379cfad9bbbdc5
Rafael Aguero Baquero <raguero@quanam.com>
2024-01-29 10:56:28 -0300

    [ADD] DESEMPEÑO

--- onsc_desempeno/models/oncs_legajo_cambio_uo.py
+++ onsc_desempeno/models/oncs_legajo_cambio_uo.py
@@ -17,7 +17,7 @@ class ONSCLegajoCambioUO(models.Model):
         EvaluationListLine = self.env['onsc.desempeno.evaluation.list.line'].suspend_security()
         new_job = super(ONSCLegajoCambioUO, self)._action_confirm()
         # OBTENIENDO COLABORADORES
-        evaluation_list_lines = EvaluationListLine.with_context(active_test=False).search([
+        evaluation_list_lines = EvaluationListLine.with_context(active_test=False, is_from_menu=False).search([
             ('evaluation_list_id.state', '=', 'in_progress'),
             ('evaluation_list_id.evaluation_stage_id.start_date', '<=', self.start_date),
             ('evaluation_list_id.evaluation_stage_id.general_cycle_id.end_date_max', '>=', self.start_date),
