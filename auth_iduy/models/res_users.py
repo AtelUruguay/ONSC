e4ba2b39c3a14aaeb20c08ea7b899e22444ab71b
Rafael Aguero <raguero@quanam.com>
2024-03-06 15:33:39 -0300

    [HOTFIX][ID UY] ps07 12496 Error de conexión a GHE- PRODUCCIÓN

--- auth_iduy/models/res_users.py
+++ auth_iduy/models/res_users.py
@@ -88,3 +88,8 @@ class ResUsers(models.Model):
         return self.env['res.country'].search([
             ('code', 'in', [country_code.upper(), country_code.lower()])
         ], limit=1)
+
+    def _get_session_token_fields(self):
+        super(ResUsers, self)._get_session_token_fields()
+        return {'id', 'login', 'password', 'active'}
+
