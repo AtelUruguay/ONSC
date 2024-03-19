6074be3f695397e5ad74cba65ecffa2685effc94
svillanueva <svillanueva@quanam.com>
2024-03-19 15:49:31 -0300

    LED_1.2_01 Crear catálogo rol acto electoral LED_1.3_01 Crear catálogo acto electoral.

--- onsc_legajo/models/catalogs/__init__.py
+++ onsc_legajo/models/catalogs/__init__.py
@@ -1,18 +1,1 @@
 # -*- coding: utf-8 -*-
-
-from . import onsc_catalog_base
-from . import onsc_legajo_emergency
-from . import onsc_legajo_causes_discharge
-from . import onsc_legajo_document_type
-from . import onsc_legajo_health_provider
-from . import onsc_legajo_income_mechanism
-from . import onsc_legajo_regime
-from . import onsc_legajo_state_square
-from . import onsc_legajo_integration_error
-from . import onsc_legajo_reason_extinction_commission
-from . import onsc_legajo_commission_regime
-from . import onsc_legajo_security_job
-from . import onsc_legajo_budget_item
-from . import onsc_legajo_norm
-from . import onsc_legajo_office
-from . import onsc_legajo_attached_document
