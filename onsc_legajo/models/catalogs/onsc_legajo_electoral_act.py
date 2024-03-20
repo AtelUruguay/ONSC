20522fd1774ddca14174f98cfc721b3bf33ce805
Rafael Aguero <raguero@quanam.com>
2024-03-20 17:39:05 -0300

    [ADD][LEGAJO DIGITAL] Revisión

--- onsc_legajo/models/catalogs/onsc_legajo_electoral_act.py
+++ onsc_legajo/models/catalogs/onsc_legajo_electoral_act.py
@@ -9,7 +9,7 @@ _logger = logging.getLogger(__name__)
 
 class ONSCLegajoElectoralAct(models.Model):
     _name = 'onsc.legajo.electoral.act'
-    _description = "Rol acto electoral"
+    _description = "Acto electoral"
 
     name = fields.Char(string="Nombre")
     act_date = fields.Date(string="Fecha del Acto")
@@ -39,7 +39,8 @@ class ONSCLegajoElectoralAct(models.Model):
             if record.date_since_entry_control and record.date_since_consultation_control and \
                     record.date_since_entry_control < record.date_since_consultation_control:
                 raise ValidationError(_("La fecha desde de ingreso está fuera del rango de fechas de consulta"))
-            if record.date_until_entry_control and record.date_until_consultation_control and record.date_until_entry_control > record.date_until_consultation_control:
+            if record.date_until_entry_control and record.date_until_consultation_control and \
+                    record.date_until_entry_control > record.date_until_consultation_control:
                 raise ValidationError(_("La fecha hasta está fuera del rango de fechas de consulta"))
 
     @api.onchange('required')
@@ -56,14 +57,14 @@ class ONSCLegajoRoleElectoralAct(models.Model):
     _description = "Rol acto electoral"
 
     _sql_constraints = [
-        ('code_unique', 'unique(code)', _(u'Ya existe un rol con ese código')),
-        ('name_unique', 'unique(name)', _('Ya existe un rol con ese nombre'))
+        ('code_unique', 'unique(code)', _(u'Ya existe un Rol acto electoral con ese código')),
+        ('name_unique', 'unique(name)', _('Ya existe un Rol acto electoral con ese nombre'))
     ]
 
     code = fields.Char(string=u"Código", required=True)
     name = fields.Char(string="Nombre", required=True)
-    holiday_work = fields.Boolean(string=u'Corresponde asueto?')
-    allow_holidays_work = fields.Boolean(string=u'Permite seleccionar trabajo en asueto?')
+    holiday_work = fields.Boolean(string=u'¿Corresponde asueto?')
+    allow_holidays_work = fields.Boolean(string=u'¿Permite seleccionar trabajo en asueto?')
 
     @api.onchange('holiday_work')
     def _onchange_holiday_work(self):
@@ -81,4 +82,4 @@ class ONSCLegajoElectoralActTypeResponsability(models.Model):
     days = fields.Integer("Días")
 
     _sql_constraints = [('role_uniq', 'UNIQUE (electoral_act_id,role_id)',
-                         _('Ya existe mas de un rol de acto electoral, en el mismo acto electoral'))]
+                         _('Ya existe mas de un Rol de acto electoral en el mismo Acto electoral'))]
