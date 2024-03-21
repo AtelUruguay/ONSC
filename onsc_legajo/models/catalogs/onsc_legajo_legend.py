bbb0ea8d2fbe7399cfa23d02ccfb8fbd72c63acc
svillanueva <svillanueva@quanam.com>
2024-03-21 15:34:48 -0300

    [ADD][PS07 12834]LED_1.10_01 Crear configuración

--- onsc_legajo/models/catalogs/onsc_legajo_legend.py
+++ onsc_legajo/models/catalogs/onsc_legajo_legend.py
@@ -9,9 +9,9 @@ _logger = logging.getLogger(__name__)
 
 class ONSCLegajoLegend(models.Model):
     _name = 'onsc.legajo.legend'
-    _description = "Acto electoral"
+    _description = "Leyenda"
 
-    legend = fields.Char(string="Texto de leyenda", required=True)
+    name = fields.Char(string="Texto de leyenda", required=True)
     regime_id = fields.Many2one("onsc.legajo.regime", "Régimen")
     descriptor1_id = fields.Many2one("onsc.catalog.descriptor1", "Descriptor 1")
     active = fields.Boolean("Activo", default=True)
@@ -24,4 +24,4 @@ class ONSCLegajoLegend(models.Model):
             if not record.regime_id and not record.regime_id:
                 raise ValidationError(_("Se debe ingresar Régimen y/o Descriptor 1"))
             if Legend.search_count([('regime_id', '=', record.regime_id.id), ('descriptor1_id', '=', record.descriptor1_id.id), ('id', '!=', record.id)]) > 0:
-                raise ValidationError(_("Ya existe una legenda para el Régimen y Descriptor 1 ingresado"))
+                raise ValidationError(_("Ya existe una Leyenda para el Régimen y Descriptor 1 ingresado"))
