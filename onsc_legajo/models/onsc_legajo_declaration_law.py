e14ff5a11590a832182b9b4bd7982d3599a974d2
Carmen Pino <carmenmpino85@gmail.com>
2024-04-07 15:36:38 -0400

    Correcciones en LED_10.1_01

--- onsc_legajo/models/onsc_legajo_declaration_law.py
+++ onsc_legajo/models/onsc_legajo_declaration_law.py
@@ -9,8 +9,9 @@ class ONSCLegajoDeclarationLaw(models.Model):
     _description = 'Declaration Law'
 
     declaration_date = fields.Date(string="Fecha de declaración", required=True)
-    digital_doc  = fields.Binary(string="Documento digitalizado", required=True)
-    legajo_id = fields.Many2one(comodel_name="onsc.legajo", string="Legajo")
+    digital_file = fields.Binary(string="Documento digitalizado", required=True)
+    digital_filename = fields.Char("Nombre del documento digitalizado")
+    legajo_id = fields.Many2one(comodel_name="onsc.legajo", string="Legajo", required=True)
 
     @api.constrains("declaration_date")
     def _check_date(self):
