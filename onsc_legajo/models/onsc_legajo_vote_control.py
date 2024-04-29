4f8e0454b3a586c4e4b8f6cb920d97785da8f369
Rafael Aguero <raguero@quanam.com>
2024-04-29 16:14:01 -0300

    [ADD][LED] LED 9 CONTROL DE VOTOS

--- onsc_legajo/models/onsc_legajo_vote_control.py
+++ onsc_legajo/models/onsc_legajo_vote_control.py
@@ -1,31 +1,66 @@
 # -*- coding: utf-8 -*-
 import json
+
 from odoo.addons.onsc_base.onsc_useful_tools import get_onchange_warning_response as warning_response
 
-from odoo import fields, models, api
+from odoo import fields, models, api, _
 from odoo.exceptions import ValidationError
+from odoo.osv import expression
 
 
 class ONSCLegajoVoteRegistry(models.Model):
     _name = "onsc.legajo.vote.registry"
     _description = 'Registro de control de votos'
+    _rec_name = "employee_id"
     _inherit = [
-        'onsc.legajo.abstract.opbase.security'
+        'onsc.legajo.abstract.legajo.security'
     ]
 
-    def _get_domain(self, args):
-        return super(ONSCLegajoVoteRegistry, self)._get_domain(args, use_employee=True)
+    def read(self, fields=None, load="_classic_read"):
+        Employee = self.env['hr.employee'].sudo()
+        result = super(ONSCLegajoVoteRegistry, self).read(fields, load)
+        for item in result:
+            if item.get('employee_id'):
+                employee_id = item['employee_id'][0]
+                item['employee_id'] = (item['employee_id'][0], Employee.browse(employee_id)._custom_display_name())
+        return result
 
-    def _is_group_inciso_security(self):
+    def _get_abstract_config_security(self):
+        return self.user_has_groups(
+            'onsc_legajo.group_legajo_vote_control_consulta,onsc_legajo.group_legajo_vote_control_administrar')
+
+    def _get_abstract_inciso_security(self):
         return self.user_has_groups('onsc_legajo.group_legajo_vote_control_recursos_humanos_inciso')
 
-    def _is_group_ue_security(self):
+    def _get_abstract_ue_security(self):
         return self.user_has_groups('onsc_legajo.group_legajo_vote_control_recursos_humanos_ue')
+
+    def _get_abstract_responsable_uo(self):
+        return self.user_has_groups('onsc_legajo.group_legajo_vote_control_responsable_uo')
+
+    @api.model
+    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+        if self._context.get('restrict_period'):
+            ElectoralAct = self.env['onsc.legajo.electoral.act'].suspend_security().with_context(active_test=False)
+            electoral_act_ids = ElectoralAct.search([
+                ('date_since_consultation_control', '<=', fields.Date.today()),
+                ('date_until_consultation_control', '>=', fields.Date.today())])
+            args = expression.AND([[('electoral_act_ids', 'in', electoral_act_ids.ids)], args])
+        return super(ONSCLegajoVoteRegistry, self)._search(
+            args,
+            offset=offset,
+            limit=limit,
+            order=order,
+            count=count,
+            access_rights_uid=access_rights_uid)
 
     employee_id = fields.Many2one(
         comodel_name="hr.employee",
         string="Funcionario",
+        required=True,
         index=True)
+    employee_id_domain = fields.Char(string="Dominio Funcionario", compute='_compute_employee_id_domain',
+                                     default=lambda self: self._get_domain_employee_ids())
     legajo_id = fields.Many2one(
         comodel_name="onsc.legajo",
         string="Legajo",
@@ -34,7 +69,7 @@ class ONSCLegajoVoteRegistry(models.Model):
     )
     date = fields.Date(
         string='Fecha de presentación',
-        default = lambda s: fields.Date.today(),
+        default=lambda s: fields.Date.today(),
         required=True)
     electoral_act_ids = fields.Many2many(
         comodel_name='onsc.legajo.electoral.act',
@@ -49,6 +84,8 @@ class ONSCLegajoVoteRegistry(models.Model):
         '¿Alguna Elección activa?',
         search='_search_any_electoral_act_active',
         store=False)
+    should_disable_form_edit = fields.Boolean(string="Deshabilitar botón de editar",
+                                              compute='_compute_should_disable_form_edit')
 
     @api.constrains("date")
     def _check_date(self):
@@ -65,11 +102,32 @@ class ONSCLegajoVoteRegistry(models.Model):
     def _search_any_electoral_act_active(self, operator, operand):
         ElectoralAct = self.env['onsc.legajo.electoral.act'].suspend_security().with_context(active_test=False)
         electoral_act_ids = ElectoralAct.search([
-                ('date_since_entry_control', '<=', fields.Date.today()),
-                ('date_until_entry_control', '>=', fields.Date.today())])
+            ('date_since_entry_control', '<=', fields.Date.today()),
+            ('date_until_entry_control', '>=', fields.Date.today())])
         return [('electoral_act_ids', 'in', electoral_act_ids.ids)]
 
     @api.depends('employee_id')
     def _compute_legajo_id(self):
         for rec in self:
             rec.legajo_id = self.env['onsc.legajo'].sudo().search([('employee_id', '=', rec.employee_id.id)], limit=1)
+
+    def _compute_employee_id_domain(self):
+        for rec in self:
+            rec.employee_id_domain = self._get_domain_employee_ids()
+
+    def _compute_should_disable_form_edit(self):
+        is_valid_user = self.user_has_groups(
+            'onsc_legajo.group_legajo_vote_control_administrar,onsc_legajo.group_legajo_vote_control_recursos_humanos_inciso,onsc_legajo.group_legajo_vote_control_recursos_humanos_ue')
+        for record in self:
+            record.should_disable_form_edit = self._context.get('is_from_menu') and not is_valid_user
+
+    def _get_domain_employee_ids(self):
+        available_contracts = self._get_user_available_contract()
+        if not available_contracts:
+            return json.dumps([('id', '=', False)])
+        else:
+            sql_query = """SELECT DISTINCT employee_id FROM hr_contract WHERE id IN %s AND employee_id IS NOT NULL"""
+            self.env.cr.execute(sql_query, [tuple(available_contracts.ids)])
+            results = self.env.cr.fetchall()
+            employee_ids = [item[0] for item in results]
+            return json.dumps([('id', 'in', employee_ids)])
