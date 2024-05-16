80687cd873555f2cf1d3fc090bf358375155d812
svillanueva <svillanueva@quanam.com>
2024-05-16 16:52:40 -0300

    LED_4.3_01 Ajustar formulario de validación documental para incluir investigación. LED_5.3_01 Ajustar formulario para incluir la solapa de investigación.

--- onsc_cv_digital_legajo/models/legajo_info/onsc_legajo_volunteering.py
+++ onsc_cv_digital_legajo/models/legajo_info/onsc_legajo_volunteering.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from odoo import Command
 from odoo import fields, models
 
 HISTORY_COLUMNS = [
@@ -28,6 +29,16 @@ TREE_HISTORY_COLUMNS = [
 
 
 class ONSCLegajoVolunteering(models.Model):
+    _inherit = 'onsc.cv.volunteering'
+    _legajo_model = 'onsc.legajo.volunteering'
+
+    def _update_legajo_record_vals(self, vals):
+        if 'volunteering_task_ids' in vals:
+            vals['volunteering_task_ids'] = [Command.clear()] + vals['volunteering_task_ids']
+        return vals
+
+
+class ONSCCVDigitalVolunteering(models.Model):
     _name = 'onsc.legajo.volunteering'
     _inherit = ['onsc.cv.volunteering', 'model.history']
     _description = 'Legajo - Voluntariado'
