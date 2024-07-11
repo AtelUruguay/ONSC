22735ca1cc3416ab8413b44fd67eda1bc1535b98
Rafael Aguero <raguero@quanam.com>
2024-07-11 18:35:40 -0300

    [FIX] Variable innecesaria

--- onsc_cv_digital_legajo/models/res_company.py
+++ onsc_cv_digital_legajo/models/res_company.py
@@ -2,14 +2,8 @@
 
 from odoo import fields, models
 
-DNIC_DOC_TYPE = [
-    ('DO', 'DO')
-]
-
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
-
-
 
     cv_help_contacts = fields.Char('Contactos')
