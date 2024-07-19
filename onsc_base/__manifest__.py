44b4c6e26480d6a096e82ffc33b35e35a84aceaf
Rafael Aguero <raguero@quanam.com>
2024-07-19 14:05:30 -0300

    c/cd

--- onsc_base/__manifest__.py
+++ onsc_base/__manifest__.py
@@ -23,7 +23,7 @@ ONSC - Base
         'fields_many2one_restrict',
         'base_restrict_access',
         'url_access_restriction',
-        'field_binary_show_filename'
+        'field_binary_show_filename',
         'disable_userprofile_menuitems',
         'disable_mail_message_options',
         'web_dialog_confirm_disable_button'
