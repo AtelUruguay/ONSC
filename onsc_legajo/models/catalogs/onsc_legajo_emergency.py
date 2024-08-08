5ac9bce31e3afe7795e69fff38cdbb26dad5b4cd
Rafael Aguero <raguero@quanam.com>
2024-08-08 11:18:29 -0300

    [FIX] ps07 13385 No usar el sequence al default sino al create para evitar perdidas de secuenciales

--- onsc_legajo/models/catalogs/onsc_legajo_emergency.py
+++ onsc_legajo/models/catalogs/onsc_legajo_emergency.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ONSCLegajoEmergency(models.Model):
     _name = 'onsc.legajo.emergency'
     _description = 'Emergencia'
 
-    code = fields.Char(u"Código", required=True,
-                       default=lambda self: self.env['ir.sequence'].next_by_code('onsc.legajo.emergency.code'))
+    code = fields.Char(u"Código", required=True)
     name = fields.Char("Nombre de la emergencia", required=True)
     phone = fields.Char(u"Teléfono")
     active = fields.Boolean('Activo', default=True)
+
+    @api.model
+    def create(self, values):
+        if 'code' not in values or ('code' in values and values.get('code') is False):
+            values['code'] = self.env['ir.sequence'].next_by_code('onsc.legajo.emergency.code')
+        return super(ONSCLegajoEmergency, self).create(values)
 
     _sql_constraints = [
         ('code_uniq', 'unique(code)', u'El código de la emergencia debe ser único'),
