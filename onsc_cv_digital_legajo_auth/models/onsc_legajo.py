7fdc3536f5e899a7f540cba728dd869b5a1adfce
Rafael Aguero <raguero@quanam.com>
2024-11-12 10:59:23 -0300

    [ADD][LEGAJO] PS07 13898 Actualizar DNIC por GH

--- onsc_cv_digital_legajo_auth/models/onsc_legajo.py
+++ onsc_cv_digital_legajo_auth/models/onsc_legajo.py
@@ -11,5 +11,8 @@ class ONSCLegajo(models.Model):
     def button_actualizar_dnic(self):
         self.cv_digital_id.button_actualizar_dnic()
         self.cv_digital_id.cv_full_name = self.cv_digital_id.partner_id.cv_full_name
+        self.cv_digital_id.cv_birthdate = self.cv_digital_id.partner_id.cv_birthdate
         self.employee_id.full_name = self.cv_digital_id.cv_full_name
+        self.employee_id.cv_birthdate = self.cv_digital_id.partner_id.cv_birthdate
+        self.employee_id.cv_sex = self.cv_digital_id.partner_id.cv_sex
         return True
