c3cafc75ae80945df8430524f36c7fd2f0683774
Rafael Aguero <raguero@quanam.com>
2024-12-10 15:30:02 -0300

    [ADD][DESEMPENO] PS07 14043 Sacar botón editar y agregar un lapiz en comentarios generales

--- onsc_desempeno/wizard/__init__.py
+++ onsc_desempeno/wizard/__init__.py
@@ -5,3 +5,4 @@ from . import onsc_desempeno_evaluator_change_wizard
 from . import onsc_desempeno_extend_deadline_wizard
 from . import onsc_desempeno_evaluator_cancel_wizard
 from . import onsc_desempeno_evaluation_report_wizard
+from . import onsc_desempeno_general_note
---
+++ onsc_desempeno/wizard/onsc_desempeno_general_note.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class ONSCDesempenoEvalaluatiorChangeWizard(models.TransientModel):
+    _name = 'onsc.desempeno.general.note'
+    _description = 'Adicionar nota general'
+
+    evaluation_id = fields.Many2one('onsc.desempeno.evaluation', string='Evaluación')
+    message = fields.Text(string="Comentarios generales")
+
+    def action_confirm(self):
+        self.evaluation_id.write({'general_comments': self.message})
+        return True
