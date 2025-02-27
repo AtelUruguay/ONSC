e4c99c2d42fb43edf09ed63611420f096451eb3a
Rafael Aguero <raguero@quanam.com>
2025-02-27 11:18:05 -0300

    [UPDATE][DESMPEÑO] PS07 14665 Refactor skill line uniqueness constraint, enhance frequency equivalence model, and improve evaluation report logic

--- onsc_desempeno/models/catalogs/onsc_frequency_equivalence.py
+++ onsc_desempeno/models/catalogs/onsc_frequency_equivalence.py
@@ -9,10 +9,12 @@ _logger = logging.getLogger(__name__)
 class ONSCDesempenoGradequivalence(models.Model):
     _name = 'onsc.desempeno.grade.equivalence'
     _description = 'Equivalencia de frecuencia'
+    _rec_name = 'degree_id'
 
     degree_id = fields.Many2one('onsc.desempeno.degree', string='Grado de Necesidad de Desarrollo', required=True)
     min_value = fields.Float(string='Mínimo', required=True)
     max_value = fields.Float(string='Máximo', required=True)
+    active = fields.Boolean(string='Activo', default=True)
 
     _sql_constraints = [
         ("degree_id_check", "unique(degree_id)", "El grado no se puede configurar más de una ocasión"),
@@ -34,7 +36,11 @@ class ONSCDesempenoGradequivalence(models.Model):
             recordset: The first record that matches the search criteria, or an
             empty recordset if no match is found.
         """
-        return self.search([('min_value', '<', value), ('max_value', '>=', value)], limit=1)
+        if value == float(0):
+            args = [('min_value', '=', 0)]
+        else:
+            args = [('min_value', '<=', value), ('max_value', '>=', value)]
+        return self.search(args, limit=1)
 
 class ONSCDesempenoFrequencyEquivalence(models.Model):
     _name = 'onsc.desempeno.frequency.equivalence'
@@ -46,6 +52,7 @@ class ONSCDesempenoFrequencyEquivalence(models.Model):
         required=True
     )
     value = fields.Float(string='Valor', required=True)
+    active = fields.Boolean(string='Activo', default=True)
 
     _sql_constraints = [
         ("frequency_check", "unique(name)", "La frecuencia no se puede configurar más de una ocasión"),
