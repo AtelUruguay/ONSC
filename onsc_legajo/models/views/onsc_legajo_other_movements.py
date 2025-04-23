6c9fb813abd0a2b0b556f3081f5d89ddd73c714a
svillanueva <svillanueva@quanam.com>
2025-04-23 13:17:41 -0300

    [ADD][14876]LER_10.1_02 Visualizar en la vista lista dependiendo los parámetros del wizard.

--- onsc_legajo/models/views/onsc_legajo_other_movements.py
+++ onsc_legajo/models/views/onsc_legajo_other_movements.py
@@ -18,9 +18,9 @@ MOV = [
 
 
 class ONSCLegajoPersonMovements(models.Model):
-    _name = "onsc.legajo.person.movements"
-    _description = "Movimientos para una persona"
-    _order = "date_start"
+    _name = "onsc.legajo.other.movements"
+    _description = "Otros movimientos de la UE/Inciso"
+
 
     token = fields.Char(string='Token', index=True)
     report_user_id = fields.Integer(string='Usuario que dió origen al reporte', index=True)
@@ -28,60 +28,34 @@ class ONSCLegajoPersonMovements(models.Model):
     # CONTRACT INFO
     nro_doc = fields.Char(u'Número de documento')
     employee_id = fields.Many2one('hr.employee', string="Funcionario")
+    move_type = fields.Selection(MOV, string='Tipo de Movimiento')
+    audit_date = fields.Date(string=u'Fecha de auditoría')
+    from_date = fields.Date(string=u'Fecha desde del movimiento')
     inciso_id = fields.Many2one('onsc.catalog.inciso', string='Inciso')
     operating_unit_id = fields.Many2one("operating.unit", string="Unidad ejecutora")
     department_id = fields.Many2one('hr.department', string="UO")
-    contract_legajo_state = fields.Selection([
-        ('active', 'Activo'),
-        ('baja', 'Baja'),
-        ('reserved', 'Reservado'),
-        ('outgoing_commission', 'Comisión saliente'),
-        ('incoming_commission', 'Comisión entrante')], string='Estado del contrato')
-    date_start = fields.Date(string=u'Fecha de alta')
-    is_uo_manager = fields.Boolean(string='¿Es responsable de UO?')
+    nroPuesto = fields.Char(string='Puesto')
+    nroPlaza = fields.Char(string='Plaza')
+    secPlaza = fields.Char(string="Sec Plaza")
     regime_id = fields.Many2one('onsc.legajo.regime', string='Régimen')
-    commission_regime_id = fields.Many2one('onsc.legajo.commission.regime', string='Régimen comisión')
-    date_end = fields.Date(string=u'Fecha de baja')
-    contract_id = fields.Many2one('hr.contract', string="Contrato")
-    inciso_origin_id = fields.Many2one('onsc.catalog.inciso', string='Inciso origen')
-    operating_unit_origin_id = fields.Many2one(
-        "operating.unit",
-        string="Unidad ejecutora origen",
-
-    )
-    inciso_dest_id = fields.Many2one(
-        'onsc.catalog.inciso',
-        string='Inciso destino',
-
-    )
-    operating_unit_dest_id = fields.Many2one(
-        "operating.unit",
-        string="Unidad ejecutora destino",
-
-    )
     descriptor1_id = fields.Many2one('onsc.catalog.descriptor1', string='Descriptor1', )
     descriptor2_id = fields.Many2one('onsc.catalog.descriptor2', string='Descriptor2')
     descriptor3_id = fields.Many2one('onsc.catalog.descriptor3', string='Descriptor3')
     descriptor4_id = fields.Many2one('onsc.catalog.descriptor4', string='Descriptor4')
     public_admin_entry_date = fields.Date(string=u'Fecha ingreso administración pública ')
-    first_operating_unit_entry_date = fields.Date(string=u'Fecha de ingreso UE')
-    date_end_commission = fields.Date(string=u'Fecha hasta de la comisión')
-    reason_description = fields.Char(string='Motivo de alta')
-    reason_deregistration = fields.Char(string='Motivo de baja')
-    income_mechanism_id = fields.Many2one('onsc.legajo.income.mechanism', string='Mecanismo de ingreso')
-    causes_discharge_id = fields.Many2one("onsc.legajo.causes.discharge", string="Causal de egreso")
-    extinction_commission_id = fields.Many2one("onsc.legajo.reason.extinction.commission",
-                                               string="Motivo de extinción de la comisión")
-    legajo_id = fields.Many2one('onsc.legajo', string="Funcionario")
+    retributive_day_id = fields.Many2one('onsc.legajo.jornada.retributiva', string='Jornada retributiva')
+    graduation_date = fields.Date(string='Fecha de graduación')
+    marital_status_id = fields.Many2one("onsc.cv.status.civil", string="Estado civil")
+    operating_unit_origin_id = fields.Many2one(
+        "operating.unit",
+        string="UE anterior",
 
-    history_id = fields.Many2one('hr.contract.state.transaction.history', string="Transacción (PARA TEST)")
-    transaction_date = fields.Date(string='Fecha de transacción (PARA TEST)')
-    hierarchical_level_id = fields.Many2one("onsc.catalog.hierarchical.level", string="Nivel de UO")
-    date_start_commission = fields.Date(string=u'Fecha desde la comisión')
-    origin_department_id = fields.Many2one('hr.department', string="UO Origen")
-    target_department_id = fields.Many2one('hr.department', 'UO Destino')
-    move_type = fields.Selection(MOV, string='Tipo de Movimiento')
-    transaction_date = fields.Date(string='Fecha de transacción (PARA TEST)')
-    legajo_state_id = fields.Many2one(
-        'onsc.legajo.res.country.department',
-        string='Departamento donde desempeña funciones')
+    )
+    nroPuesto_origin = fields.Char(string='Puesto anterior')
+    nroPlaza_origin = fields.Char(string='Plaza anterior')
+    secPlaza_origin = fields.Char(string="Sec Plaza anterior")
+    descriptor1_origin_id = fields.Many2one('onsc.catalog.descriptor1', string='Descriptor1 anterior')
+    descriptor2_origin_id = fields.Many2one('onsc.catalog.descriptor2', string='Descriptor2 anterior')
+    descriptor3_origin_id = fields.Many2one('onsc.catalog.descriptor3', string='Descriptor3 anterior')
+    descriptor4_origin_id = fields.Many2one('onsc.catalog.descriptor4', string='Descriptor4 anterior')
+
