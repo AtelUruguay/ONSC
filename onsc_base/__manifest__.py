5a3a5d738924deee899f367a7b81cc4c3f1dfe3e
Rafael Aguero <raguero@quanam.com>
2025-04-29 13:28:38 -0300

    [VERSIONADO]

--- onsc_base/__manifest__.py
+++ onsc_base/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ONSC - Base',
-    'version': '15.0.32.0.0',
+    'version': '15.0.34.0.0',
     'summary': 'ONSC - Base',
     'sequence': 11,
     'description': """
