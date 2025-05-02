7a2e3b2d5644a216750c1b19276c07cd3564eed3
Rafael Aguero <raguero@quanam.com>
2025-05-02 13:42:01 -0300

    [VERSIONADO] Update version number to 15.0.31.2.0 and modify label for last sync date in legajo views

--- onsc_base/__manifest__.py
+++ onsc_base/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ONSC - Base',
-    'version': '15.0.31.1.0',
+    'version': '15.0.31.2.0',
     'summary': 'ONSC - Base',
     'sequence': 11,
     'description': """
